8851ee5f413666bc158487d27ba47be5fdc56edb
marc <marc@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-01-13 20:25:44 +0000

    Function to center a frame against a parentframe
    git-svn-id: svn://svn.appwork.org/utils@5 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/utils/swing/SwingUtils.java
@@ -0,0 +1,31 @@
+package org.appwork.utils.swing;
+
+import java.awt.Point;
+
+import javax.swing.JFrame;
+
+public class SwingUtils {
+    /**
+     * Calculates the position of a frame to be in the center of an other frame.
+     * 
+     * @param parentFrame
+     * @param frame
+     * @return
+     */
+    public static Point getCenter(JFrame parentFrame, JFrame frame) {
+        Point point = new Point();
+        int x = 0, y = 0;
+
+        if (parentFrame == null || frame == null) {
+            point.setLocation(x, y);
+            return point;
+        }
+
+        x = parentFrame.getLocation().x + (parentFrame.getSize().width / 2) - (frame.getSize().width / 2);
+        y = parentFrame.getLocation().y + (parentFrame.getSize().height / 2) - (frame.getSize().height / 2);
+
+        point.setLocation(x, y);
+
+        return point;
+    }
+}
