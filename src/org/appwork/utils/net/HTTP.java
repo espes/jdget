4bd7eabfd1894b3a192921556804fd0997b6fd3e
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-01-15 21:57:07 +0000

    git-svn-id: svn://svn.appwork.org/utils@21 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/utils/net/HTTP.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2009 - 2010 AppWork UG(haftungsbeschränkt) <e-mail@appwork.org>
+ * 
+ * This file is part of org.appwork.utils.net
+ * 
+ * This software is licensed under the Artistic License 2.0,
+ * see the LICENSE file or http://www.opensource.org/licenses/artistic-license-2.0.php
+ * for details
+ */
+package org.appwork.utils.net;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * @author thomas
+ * 
+ */
+public class HTTP {
+
+    /**
+     * Downloads a file and stores data to a file
+     * 
+     * @param url
+     * @param cache
+     * @throws IOException
+     */
+    public static void download(URL url, File file) throws IOException {
+
+        final File parentFile = file.getParentFile();
+        if (parentFile != null && !parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        file.createNewFile();
+
+        final BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(file, true));
+        BufferedInputStream input;
+        URLConnection con = url.openConnection();
+        if (url.openConnection().getHeaderField("Content-Encoding") != null && con.getHeaderField("Content-Encoding").equalsIgnoreCase("gzip")) {
+            input = new BufferedInputStream(new GZIPInputStream(con.getInputStream()));
+        } else {
+            input = new BufferedInputStream(con.getInputStream());
+        }
+
+        final byte[] b = new byte[1024];
+        int len;
+        while ((len = input.read(b)) != -1) {
+            output.write(b, 0, len);
+        }
+        output.close();
+        input.close();
+
+    }
+
+}
