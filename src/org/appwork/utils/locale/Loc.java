b4c123a713dbc7d8377c096ad779435bb7d2c22a
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-08-04 14:13:12 +0000

    git-svn-id: svn://svn.appwork.org/utils@249 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/locale/Loc.java
+++ src/org/appwork/utils/locale/Loc.java
@@ -31,35 +31,126 @@ public class Loc {
 
     public static final Storage CFG = JSonStorage.getStorage("Locale");
     /**
-     * The key (String) under which the saved localization-name is stored.
+     * The HashMap which contains all hashcodes of the keys and their translated
+     * values.
+     * 
+     * @see Loc#parseLocalization(RFSFile)
      */
-    public static final String PROPERTY_LOCALE = "PROPERTY_LOCALE2";
+    private static HashMap<Integer, String> DATA = null;
+
+    private static String DEFAULT_LOCALE_CACHE;
 
     /**
-     * The directory, where all localization files are located.
+     * The default localization file. This is the english language.
      */
-    public static final File LOCALIZATION_DIR = Application.getRessource("languages/");
+    private static final File DEFAULT_LOCALIZATION = new File(Loc.LOCALIZATION_DIR, Loc.getDefaultLocale() + ".loc");
 
     /**
      * The name of the default localization file. This is the english language.
      */
     private static final String FALLBACK_LOCALE = "en_GB";
 
-    /**
-     * The default localization file. This is the english language.
-     */
-    private static final File DEFAULT_LOCALIZATION = new File(LOCALIZATION_DIR, getDefaultLocale() + ".loc");
-
-    /**
-     * The HashMap which contains all hashcodes of the keys and their translated
-     * values.
-     * 
-     * @see Loc#parseLocalization(RFSFile)
-     */
-    private static HashMap<Integer, String> data = null;
-
     private static String locale;
-    private static String DEFAULT_LOCALE_CACHE;
+
+    /**
+     * The directory, where all localization files are located.
+     */
+    public static final File LOCALIZATION_DIR = Application.getRessource("languages/");
+    /**
+     * The key (String) under which the saved localization-name is stored.
+     */
+    public static final String PROPERTY_LOCALE = "PROPERTY_LOCALE2";
+
+    /**
+     * @return
+     */
+    private static String getDefaultLocale() {
+        if (Loc.DEFAULT_LOCALE_CACHE != null) { return Loc.DEFAULT_LOCALE_CACHE; }
+        final String sys = System.getProperty("user.language").toLowerCase();
+        final String cou = System.getProperty("user.country").toUpperCase();
+
+        final String[] locs = Loc.getLocales();
+        if (locs.length == 0) {
+            Loc.DEFAULT_LOCALE_CACHE = Loc.FALLBACK_LOCALE;
+
+        }
+        if (Loc.DEFAULT_LOCALE_CACHE == null) {
+            for (final String l : locs) {
+
+                if (l.equals(sys + "_" + cou)) {
+                    Loc.DEFAULT_LOCALE_CACHE = l;
+                    break;
+                }
+            }
+        }
+        if (Loc.DEFAULT_LOCALE_CACHE == null) {
+            for (final String l : locs) {
+
+                if (l.equals(sys)) {
+                    Loc.DEFAULT_LOCALE_CACHE = l;
+                    break;
+                }
+            }
+        }
+        if (Loc.DEFAULT_LOCALE_CACHE == null) {
+            for (final String l : locs) {
+                if (l.startsWith(sys + "_")) {
+                    Loc.DEFAULT_LOCALE_CACHE = l;
+                    break;
+                }
+            }
+        }
+
+        if (Loc.DEFAULT_LOCALE_CACHE == null) {
+            for (final String l : locs) {
+                if (l.equals(Loc.FALLBACK_LOCALE)) {
+                    Loc.DEFAULT_LOCALE_CACHE = l;
+                    break;
+                }
+            }
+        }
+        if (Loc.DEFAULT_LOCALE_CACHE == null) {
+            Loc.DEFAULT_LOCALE_CACHE = locs[0];
+        }
+        return Loc.DEFAULT_LOCALE_CACHE;
+    }
+
+    /**
+     * Returns a localized regular expression for words that usualy ar present
+     * in an error message
+     * 
+     * @return
+     */
+    public static String getErrorRegex() {
+        return Loc.L("system.error", ".*(error|failed).*");
+    }
+
+    /**
+     * @return
+     */
+    public static String getLocale() {
+
+        return Loc.locale;
+    }
+
+    /**
+     * @return
+     */
+    public static String[] getLocales() {
+        final String[] files = Loc.LOCALIZATION_DIR.list(new FilenameFilter() {
+
+            public boolean accept(final File dir, final String name) {
+                return name.endsWith(".loc");
+
+            }
+
+        });
+        if (files == null) { return new String[] {}; }
+        for (int i = 0; i < files.length; i++) {
+            files[i] = files[i].replace(".loc", "");
+        }
+        return files;
+    }
 
     /**
      * Returns the translated value for the translation-key. If the current
@@ -78,23 +169,23 @@ public class Loc {
      * @throws IllegalArgumentException
      *             if the key is null or is empty
      */
-    public static String L(String key, String def) {
-        if (key == null || (key = key.trim()).length() == 0) throw new IllegalArgumentException();
-        if (data == null) {
+    public static String L(String key, final String def) {
+        if ((key == null) || ((key = key.trim()).length() == 0)) { throw new IllegalArgumentException(); }
+        if (Loc.DATA == null) {
             Log.L.warning("No parsed localization found! Loading now from saved localization file!");
             try {
-                Loc.setLocale(CFG.get(PROPERTY_LOCALE, FALLBACK_LOCALE));
-            } catch (Exception e) {
+                Loc.setLocale(Loc.CFG.get(Loc.PROPERTY_LOCALE, Loc.FALLBACK_LOCALE));
+            } catch (final Exception e) {
 
                 Log.L.severe("Error while loading the stored localization name!");
-                Loc.setLocale(FALLBACK_LOCALE);
+                Loc.setLocale(Loc.FALLBACK_LOCALE);
             }
-            if (data == null) return def == null ? "Error in Loc! No loaded data!" : def;
+            if (Loc.DATA == null) { return def == null ? "Error in Loc! No loaded data!" : def; }
         }
 
-        String loc = data.get(key.toLowerCase().hashCode());
+        final String loc = Loc.DATA.get(key.toLowerCase().hashCode());
         if (loc == null) {
-            data.put(key.toLowerCase().hashCode(), def);
+            Loc.DATA.put(key.toLowerCase().hashCode(), def);
             return def;
         }
         return loc;
@@ -116,11 +207,65 @@ public class Loc {
      * @return translated value or the def parameter filled with the parameters
      * @see Loc#L(String, String)
      */
-    public static String LF(String key, String def, Object... args) {
+    public static String LF(final String key, final String def, final Object... args) {
         try {
-            return String.format(L(key, def), args);
-        } catch (Exception e) {
+            return String.format(Loc.L(key, def), args);
+        } catch (final Exception e) {
             return "Error: " + key;
+        }
+    }
+
+    /**
+     * Creates a HashMap with the data obtained from the localization file. <br>
+     * <b>Warning:</b> Overwrites any previously created HashMap
+     * 
+     * @param file
+     *            {@link RFSFile} object to the localization file
+     * @throws IllegalArgumentException
+     *             if the parameter is null or doesn't exist
+     * @see Loc#DATA
+     */
+    public static void parseLocalization(final File file) throws IllegalArgumentException {
+        if ((file == null) || !file.exists()) { throw new IllegalArgumentException(); }
+
+        if (Loc.DATA != null) {
+            Log.L.finer("Previous HashMap will be overwritten!");
+        }
+        Loc.DATA = new HashMap<Integer, String>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+
+            String line;
+            String key;
+            String value;
+            int split;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    continue;
+                }
+
+                if ((split = line.indexOf('=')) <= 0) {
+                    continue;
+                }
+
+                key = line.substring(0, split).toLowerCase().trim();
+                value = line.substring(split + 1).trim();
+                value = value.replace("\\n", "\n").replace("\\r", "\r");
+
+                Loc.DATA.put(key.hashCode(), value);
+            }
+        } catch (final Exception e) {
+            org.appwork.utils.logging.Log.exception(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final Exception e) {
+                    org.appwork.utils.logging.Log.exception(e);
+                }
+            }
         }
     }
 
@@ -135,167 +280,30 @@ public class Loc {
         try {
             if (loc == null) {
 
-                loc = CFG.get(PROPERTY_LOCALE, getDefaultLocale());
+                loc = Loc.CFG.get(Loc.PROPERTY_LOCALE, Loc.getDefaultLocale());
             }
-            File file = new File(LOCALIZATION_DIR, loc + ".loc");
-            locale = loc;
-            if (file != null && file.exists()) {
+            final File file = new File(Loc.LOCALIZATION_DIR, loc + ".loc");
+            Loc.locale = loc;
+            if ((file != null) && file.exists()) {
 
-                String[] locs = loc.split("_");
+                final String[] locs = loc.split("_");
                 if (locs.length == 1) {
                     Locale.setDefault(new Locale(locs[0]));
                 } else {
                     Locale.setDefault(new Locale(locs[0], locs[1]));
                 }
-                CFG.put(PROPERTY_LOCALE, loc);
+                Loc.CFG.put(Loc.PROPERTY_LOCALE, loc);
                 Loc.parseLocalization(file);
             } else {
-                Log.L.info("The language " + loc + " isn't available! Parsing default (" + FALLBACK_LOCALE + ".loc) one!");
-                locale = getDefaultLocale();
-                String[] locs = locale.split("_");
+                Log.L.info("The language " + loc + " isn't available! Parsing default (" + Loc.FALLBACK_LOCALE + ".loc) one!");
+                Loc.locale = Loc.getDefaultLocale();
+                final String[] locs = Loc.locale.split("_");
                 Locale.setDefault(new Locale(locs[0], locs[1]));
-                Loc.parseLocalization(DEFAULT_LOCALIZATION);
+                Loc.parseLocalization(Loc.DEFAULT_LOCALIZATION);
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             org.appwork.utils.logging.Log.exception(e);
         }
-    }
-
-    /**
-     * @return
-     */
-    private static String getDefaultLocale() {
-        if (DEFAULT_LOCALE_CACHE != null) return DEFAULT_LOCALE_CACHE;
-        String sys = System.getProperty("user.language").toLowerCase();
-        String cou = System.getProperty("user.country").toUpperCase();
-
-        String[] locs = getLocales();
-        if (locs.length == 0) {
-            DEFAULT_LOCALE_CACHE = FALLBACK_LOCALE;
-
-        }
-        if (DEFAULT_LOCALE_CACHE == null) {
-            for (String l : locs) {
-
-                if (l.equals(sys + "_" + cou)) {
-                    DEFAULT_LOCALE_CACHE = l;
-                    break;
-                }
-            }
-        }
-        if (DEFAULT_LOCALE_CACHE == null) {
-            for (String l : locs) {
-
-                if (l.equals(sys)) {
-                    DEFAULT_LOCALE_CACHE = l;
-                    break;
-                }
-            }
-        }
-        if (DEFAULT_LOCALE_CACHE == null) {
-            for (String l : locs) {
-                if (l.startsWith(sys + "_")) {
-                    DEFAULT_LOCALE_CACHE = l;
-                    break;
-                }
-            }
-        }
-
-        if (DEFAULT_LOCALE_CACHE == null) {
-            for (String l : locs) {
-                if (l.equals(FALLBACK_LOCALE)) {
-                    DEFAULT_LOCALE_CACHE = l;
-                    break;
-                }
-            }
-        }
-        if (DEFAULT_LOCALE_CACHE == null) DEFAULT_LOCALE_CACHE = locs[0];
-        return DEFAULT_LOCALE_CACHE;
-    }
-
-    /**
-     * Creates a HashMap with the data obtained from the localization file. <br>
-     * <b>Warning:</b> Overwrites any previously created HashMap
-     * 
-     * @param file
-     *            {@link RFSFile} object to the localization file
-     * @throws IllegalArgumentException
-     *             if the parameter is null or doesn't exist
-     * @see Loc#data
-     */
-    public static void parseLocalization(File file) throws IllegalArgumentException {
-        if (file == null || !file.exists()) throw new IllegalArgumentException();
-
-        if (data != null) Log.L.finer("Previous HashMap will be overwritten!");
-        data = new HashMap<Integer, String>();
-
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-
-            String line;
-            String key;
-            String value;
-            int split;
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith("#")) continue;
-
-                if ((split = line.indexOf('=')) <= 0) continue;
-
-                key = line.substring(0, split).toLowerCase().trim();
-                value = line.substring(split + 1).trim();
-                value = value.replace("\\n", "\n").replace("\\r", "\r");
-
-                data.put(key.hashCode(), value);
-            }
-        } catch (Exception e) {
-            org.appwork.utils.logging.Log.exception(e);
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (Exception e) {
-                    org.appwork.utils.logging.Log.exception(e);
-                }
-            }
-        }
-    }
-
-    /**
-     * Returns a localized regular expression for words that usualy ar present
-     * in an error message
-     * 
-     * @return
-     */
-    public static String getErrorRegex() {
-        return L("system.error", ".*(error|failed).*");
-    }
-
-    /**
-     * @return
-     */
-    public static String[] getLocales() {
-        String[] files = LOCALIZATION_DIR.list(new FilenameFilter() {
-
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".loc");
-
-            }
-
-        });
-        if (files == null) return new String[] {};
-        for (int i = 0; i < files.length; i++) {
-            files[i] = files[i].replace(".loc", "");
-        }
-        return files;
-    }
-
-    /**
-     * @return
-     */
-    public static String getLocale() {
-
-        return locale;
     }
 
 }
