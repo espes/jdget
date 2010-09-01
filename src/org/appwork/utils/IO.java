2d00b6fd32ebd865d42791dd42b5e380cc8cdab0
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-09-01 22:02:49 +0000

    git-svn-id: svn://svn.appwork.org/utils@313 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/IO.java
+++ src/org/appwork/utils/IO.java
@@ -11,124 +11,19 @@ import java.io.InputStreamReader;
 import java.io.Writer;
 import java.nio.channels.FileChannel;
 
+import org.appwork.utils.logging.Log;
 import org.appwork.utils.os.CrossSystem;
 
 public class IO {
-    public static String readFileToString(File file) throws IOException {
-        BufferedReader f = null;
-        InputStreamReader isr = null;
-        FileInputStream fis = null;
-        try {
-            f = new BufferedReader(isr = new InputStreamReader(fis = new FileInputStream(file), "UTF8"));
-            String line;
-            final StringBuilder ret = new StringBuilder();
-            String sep = System.getProperty("line.separator");
-            while ((line = f.readLine()) != null) {
-                ret.append(line + sep);
-            }
-            return ret.toString();
-        } finally {
-            try {
-                f.close();
-            } catch (Throwable e) {
-            }
-            try {
-                isr.close();
-            } catch (Throwable e) {
-            }
-            try {
-                fis.close();
-            } catch (Throwable e) {
-            }
-        }
-    }
-
-    public static String importFileToString(File file) throws IOException {
-        final byte[] bytes = readFile(file);
-        if (bytes == null) return null;
-        return new String(bytes, "UTF-8");
-    }
-
-    /**
-     * @param file
-     * @param string
-     */
-    public static void writeStringToFile(File file, String string) throws IOException {
-        if (file == null) { throw new IllegalArgumentException("File is null."); }
-        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
-        file.createNewFile();
-        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
-        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
-        FileWriter fw = null;
-        Writer output = new BufferedWriter(fw = new FileWriter(file));
-
-        try {
-            output.write(string);
-        } finally {
-            try {
-                output.close();
-            } catch (Throwable e) {
-            }
-            try {
-                fw.close();
-            } catch (Throwable e) {
-            }
-        }
-
-    }
-
-    /**
-     * @param tmp
-     * @param encrypt
-     * @throws IOException
-     */
-    public static void writeToFile(File file, byte[] data) throws IOException {
-        if (file == null) { throw new IllegalArgumentException("File is null."); }
-        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
-        file.createNewFile();
-        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
-        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
-
-        FileOutputStream out = new FileOutputStream(file);
-        try {
-            out.write(data);
-        } finally {
-            try {
-                out.close();
-            } catch (Throwable e) {
-            }
-        }
-    }
-
-    /**
-     * @param ressource
-     * @return
-     * @throws IOException
-     */
-    public static byte[] readFile(File ressource) throws IOException {
-        FileInputStream in = new FileInputStream(ressource);
-        byte[] bytes = null;
-        try {
-            bytes = new byte[(int) ressource.length()];
-            in.read(bytes);
-        } finally {
-            try {
-                in.close();
-            } catch (Throwable e) {
-            }
-        }
-        return bytes;
-    }
-
     /**
      * @param file
      * @param file2
      */
-    public static void copyFile(File in, File out) throws IOException {
+    public static void copyFile(final File in, final File out) throws IOException {
         FileInputStream fis = null;
         FileOutputStream fos = null;
-        FileChannel inChannel = (fis = new FileInputStream(in)).getChannel();
-        FileChannel outChannel = (fos = new FileOutputStream(out)).getChannel();
+        final FileChannel inChannel = (fis = new FileInputStream(in)).getChannel();
+        final FileChannel outChannel = (fos = new FileOutputStream(out)).getChannel();
         try {
             if (CrossSystem.isWindows()) {
                 // magic number for Windows, 64Mb - 32Kb)
@@ -141,8 +36,8 @@ public class IO {
                 //
                 // For a discussion about this see :
                 // http://forum.java.sun.com/thread.jspa?threadID=439695&messageID=2917510
-                int maxCount = (64 * 1024 * 1024) - (32 * 1024);
-                long size = inChannel.size();
+                final int maxCount = 64 * 1024 * 1024 - 32 * 1024;
+                final long size = inChannel.size();
                 long position = 0;
                 while (position < size) {
                     position += inChannel.transferTo(position, maxCount, outChannel);
@@ -151,24 +46,131 @@ public class IO {
                 inChannel.transferTo(0, inChannel.size(), outChannel);
 
             }
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw e;
         } finally {
             try {
                 outChannel.close();
-            } catch (Throwable e) {
+            } catch (final Throwable e) {
             }
             try {
                 fos.close();
-            } catch (Throwable e) {
+            } catch (final Throwable e) {
             }
             try {
                 inChannel.close();
-            } catch (Throwable e) {
+            } catch (final Throwable e) {
             }
             try {
                 fis.close();
-            } catch (Throwable e) {
+            } catch (final Throwable e) {
+            }
+        }
+    }
+
+    public static String importFileToString(final File file) throws IOException {
+        final byte[] bytes = IO.readFile(file);
+        if (bytes == null) { return null; }
+        return new String(bytes, "UTF-8");
+    }
+
+    /**
+     * @param ressource
+     * @return
+     * @throws IOException
+     */
+    public static byte[] readFile(final File ressource) throws IOException {
+        Log.L.fine("Read file: " + ressource.getAbsolutePath() + "(" + ressource.length() + " bytes)");
+        final FileInputStream in = new FileInputStream(ressource);
+        byte[] bytes = null;
+        try {
+            bytes = new byte[(int) ressource.length()];
+            in.read(bytes);
+        } finally {
+            try {
+                in.close();
+            } catch (final Throwable e) {
+            }
+        }
+        return bytes;
+    }
+
+    public static String readFileToString(final File file) throws IOException {
+        BufferedReader f = null;
+        InputStreamReader isr = null;
+        FileInputStream fis = null;
+        try {
+            f = new BufferedReader(isr = new InputStreamReader(fis = new FileInputStream(file), "UTF8"));
+            String line;
+            final StringBuilder ret = new StringBuilder();
+            final String sep = System.getProperty("line.separator");
+            while ((line = f.readLine()) != null) {
+                ret.append(line + sep);
+            }
+            return ret.toString();
+        } finally {
+            try {
+                f.close();
+            } catch (final Throwable e) {
+            }
+            try {
+                isr.close();
+            } catch (final Throwable e) {
+            }
+            try {
+                fis.close();
+            } catch (final Throwable e) {
+            }
+        }
+    }
+
+    /**
+     * @param file
+     * @param string
+     */
+    public static void writeStringToFile(final File file, final String string) throws IOException {
+        if (file == null) { throw new IllegalArgumentException("File is null."); }
+        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
+        file.createNewFile();
+        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
+        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
+        FileWriter fw = null;
+        final Writer output = new BufferedWriter(fw = new FileWriter(file));
+
+        try {
+            output.write(string);
+        } finally {
+            try {
+                output.close();
+            } catch (final Throwable e) {
+            }
+            try {
+                fw.close();
+            } catch (final Throwable e) {
+            }
+        }
+
+    }
+
+    /**
+     * @param tmp
+     * @param encrypt
+     * @throws IOException
+     */
+    public static void writeToFile(final File file, final byte[] data) throws IOException {
+        if (file == null) { throw new IllegalArgumentException("File is null."); }
+        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
+        file.createNewFile();
+        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
+        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
+
+        final FileOutputStream out = new FileOutputStream(file);
+        try {
+            out.write(data);
+        } finally {
+            try {
+                out.close();
+            } catch (final Throwable e) {
             }
         }
     }
