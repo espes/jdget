678cbc7db6c4da643e384705db075c5d0201f5bc
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-09-17 11:37:55 +0000

    Unittest for long/int conversions
    git-svn-id: svn://svn.appwork.org/utils@346 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/storage/test/StorageTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2009 - 2010 AppWork UG(haftungsbeschränkt) <e-mail@appwork.org>
+ * 
+ * This file is part of org.appwork.storage.test
+ * 
+ * This software is licensed under the Artistic License 2.0,
+ * see the LICENSE file or http://www.opensource.org/licenses/artistic-license-2.0.php
+ * for details
+ */
+package org.appwork.storage.test;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.Storage;
+import org.appwork.utils.logging.Log;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author thomas
+ * 
+ */
+public class StorageTest {
+
+    @Test
+    public void defaultTest() {
+        try {
+            // this test has to be executed several times, because it writes on
+            // app exit data to disk and evaluates it on the next start
+            final Storage s = JSonStorage.getPlainStorage("org.appwork.storage.test.StorageTest");
+            s.put("LONG", Long.MAX_VALUE);
+            final long myLong = s.get("LONG", 0l);
+            Assert.assertTrue("Restore error", myLong == Long.MAX_VALUE);
+            // should convert to -1 int
+            final int myInt = s.get("LONG", 0);
+            Assert.assertTrue("Restore error", myInt == -1);
+
+            s.put("TINYLONG", 100l);
+
+            final long mytinylong = s.get("TINYLONG", 0l);
+            Assert.assertTrue("Restore error", mytinylong == 100l);
+            // tiny long to int conversions should work
+            final long mytinyint = s.get("TINYLONG", 0);
+            Assert.assertTrue("Restore error", mytinyint == 100l);
+
+        } catch (final Exception e) {
+            Log.exception(e);
+            Assert.assertTrue(e.getMessage(), false);
+
+        }
+
+    }
+
+}
