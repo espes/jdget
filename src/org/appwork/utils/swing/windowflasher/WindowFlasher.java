70c925f132feb61a98b9ebe42623fea770e520e5
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-11-15 16:53:35 +0000

    git-svn-id: svn://svn.appwork.org/utils@365 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/utils/swing/windowflasher/WindowFlasher.java
@@ -0,0 +1,59 @@
+package org.appwork.utils.swing.windowflasher;
+
+import java.awt.Dialog;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class WindowFlasher {
+
+    private final JFrame window;
+    private final Dialog d;
+
+    /**
+     * @param frame
+     */
+    public WindowFlasher(final JFrame frame) {
+        this.window = frame;
+        this.d = new java.awt.Dialog(this.window);
+        this.d.setUndecorated(true);
+        this.d.setSize(0, 0);
+        this.d.setModal(false);
+
+        final WindowAdapter dWindowListener = new WindowAdapter() {
+
+            @Override
+            public void windowGainedFocus(final WindowEvent e) {
+                WindowFlasher.this.window.requestFocus();
+                WindowFlasher.this.d.setVisible(false);
+
+            }
+        };
+        final WindowAdapter windowWindowAdapter = new WindowAdapter() {
+
+            @Override
+            public void windowGainedFocus(final WindowEvent e) {
+                WindowFlasher.this.d.setVisible(false);
+
+            }
+        };
+        this.d.addWindowFocusListener(dWindowListener);
+        this.window.addWindowFocusListener(windowWindowAdapter);
+
+    }
+
+    /**
+     * 
+     */
+    public void start() {
+
+        if (!this.window.isFocused()) {
+            this.d.setVisible(false);
+            this.d.setLocation(0, 0);
+            this.d.setLocationRelativeTo(this.window);
+            this.d.setVisible(true);
+        }
+    }
+
+}
