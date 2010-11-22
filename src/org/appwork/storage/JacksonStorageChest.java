93669c5e70b34e2b7aa2d7489aaaf96ded97d09d
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-11-22 15:46:50 +0000

    git-svn-id: svn://svn.appwork.org/utils@373 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/storage/JacksonStorageChest.java
+++ src/org/appwork/storage/JacksonStorageChest.java
@@ -192,7 +192,21 @@ public class JacksonStorageChest extends Storage {
     public Object remove(final String key) {
         return this.map.remove(key);
     }
-
+public String toString(){
+    try {
+        return JSonStorage.getMapper().writeValueAsString(this.map);
+    } catch (JsonGenerationException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (JsonMappingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    return map.toString();
+}
     @Override
     public void save() throws StorageException {
         synchronized (JSonStorage.LOCK) {
