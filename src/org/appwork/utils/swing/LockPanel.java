3b0e330706a1ff62d2eec3eb84f2339054440074
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2010-11-23 14:15:01 +0000

    git-svn-id: svn://svn.appwork.org/utils@377 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/swing/LockPanel.java
+++ src/org/appwork/utils/swing/LockPanel.java
@@ -51,26 +51,42 @@ public class LockPanel extends JPanel {
     /**
      * 
      */
-    private static final long serialVersionUID = -2262534550090971819L;
-    private JFrame frame;
-    private Robot robot;
-    private BufferedImage screen;
-    private BufferedImage gray;
-    private Timer fadeTimer;
-    private int fadeCounter;
-    private double steps;
-    private JWindow waitingPanel;
-    private JTextArea text;
+    private static final long                       serialVersionUID = -2262534550090971819L;
+    private final JFrame                            frame;
+    private final Robot                             robot;
+    private BufferedImage                           screen;
+    private BufferedImage                           gray;
+    private Timer                                   fadeTimer;
+    private int                                     fadeCounter;
+    private double                                  steps;
+    private final JWindow                           waitingPanel;
+    private JTextArea                               text;
 
     // if there are different lockpanels for the same frame, the fade animations
     // may lock
-    private static final HashMap<JFrame, LockPanel> CACHE = new HashMap<JFrame, LockPanel>();
+    private static final HashMap<JFrame, LockPanel> CACHE            = new HashMap<JFrame, LockPanel>();
+
+    /**
+     * @param parentOwner
+     * @throws AWTException
+     */
+    public synchronized static LockPanel create(final JFrame parentOwner) throws AWTException {
+        LockPanel ret = LockPanel.CACHE.get(parentOwner);
+        if (ret == null) {
+            ret = new LockPanel(parentOwner);
+            LockPanel.CACHE.put(parentOwner, ret);
+        }
+        return ret;
+
+    }
+
+    private float alpha = 0.1f;
 
     /**
      * @param frame
      * @throws AWTException
      */
-    private LockPanel(JFrame frame) throws AWTException {
+    private LockPanel(final JFrame frame) throws AWTException {
         this.frame = frame;
 
         robot = new Robot();
@@ -78,32 +94,34 @@ public class LockPanel extends JPanel {
 
         frame.addWindowListener(new WindowListener() {
 
-            public void windowActivated(WindowEvent e) {
-                if (waitingPanel.isVisible()) waitingPanel.toFront();
+            public void windowActivated(final WindowEvent e) {
+                if (waitingPanel.isVisible()) {
+                    waitingPanel.toFront();
+                }
 
             }
 
-            public void windowClosed(WindowEvent e) {
+            public void windowClosed(final WindowEvent e) {
 
             }
 
-            public void windowClosing(WindowEvent e) {
+            public void windowClosing(final WindowEvent e) {
 
             }
 
-            public void windowDeactivated(WindowEvent e) {
+            public void windowDeactivated(final WindowEvent e) {
 
             }
 
-            public void windowDeiconified(WindowEvent e) {
+            public void windowDeiconified(final WindowEvent e) {
 
             }
 
-            public void windowIconified(WindowEvent e) {
+            public void windowIconified(final WindowEvent e) {
 
             }
 
-            public void windowOpened(WindowEvent e) {
+            public void windowOpened(final WindowEvent e) {
 
             }
 
@@ -114,7 +132,7 @@ public class LockPanel extends JPanel {
         p.setLayout(new MigLayout("ins 10", "[][fill,grow]", "[fill,grow]"));
         try {
             p.add(new JLabel(ImageProvider.getImageIcon("wait", 32, 32, true)));
-        } catch (IOException e) {
+        } catch (final IOException e) {
             Log.exception(e);
         }
         p.add(text = new JTextArea(), "spanx,aligny center");
@@ -125,141 +143,24 @@ public class LockPanel extends JPanel {
         text.setBorder(null);
         text.setBackground(null);
 
-        this.addMouseListener(new MouseAdapter() {
+        addMouseListener(new MouseAdapter() {
         });
     }
-
-    /**
-     * @return the text
-     */
-    protected JTextArea getText() {
-        return text;
-    }
-
-    /**
-     * @param time
-     */
-    public void lock(int time) {
-
-        screen = createScreenShot();
-        if (screen != null) {
-            gray = ImageProvider.convertToGrayScale(screen);
-
-            float data[] = { 0.0625f, 0.125f, 0.0625f, 0.125f, 0.25f, 0.125f, 0.0625f, 0.125f, 0.0625f };
-            Kernel kernel = new Kernel(3, 3, data);
-            ConvolveOp convolve = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
-            BufferedImage dest = new BufferedImage(gray.getWidth(), gray.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-            gray = convolve.filter(gray, dest);
-            frame.setGlassPane(this);
-
-            frame.getGlassPane().setVisible(true);
-
-            fadeIn(time);
-        }
-
-    }
-
-    public void setWaitingPanelText(String wait) {
-        if (wait == null) {
-            this.waitingPanel.setVisible(false);
-        } else {
-
-            text.setText(wait);
-            waitingPanel.pack();
-            waitingPanel.setLocation(SwingUtils.getCenter(frame, waitingPanel));
-            waitingPanel.setVisible(true);
-
-        }
-
-    }
-
-    public synchronized void fadeOut(int time) {
-        fadeCounter--;
-        steps = (50 * 1.0) / time;
-        if (fadeCounter > 0) return;
-        if (fadeTimer != null) {
-            fadeTimer.stop();
-            fadeTimer = null;
-        }
-
-        fadeTimer = new Timer(50, new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                alpha -= steps;
-                if (alpha <= 0.0) {
-                    alpha = 0.0f;
-                    if (fadeTimer != null) fadeTimer.stop();
-                    fadeTimer = null;
-                    setWaitingPanelText(null);
-
-                    frame.getGlassPane().setVisible(false);
-                }
-
-                LockPanel.this.repaint();
-            }
-        });
-        fadeTimer.setRepeats(true);
-        fadeTimer.setInitialDelay(0);
-        fadeTimer.start();
-
-    }
-
-    public synchronized void fadeIn(int time) {
-        fadeCounter++;
-        steps = (50 * 1.0) / time;
-        if (fadeTimer != null) {
-            fadeTimer.stop();
-            fadeTimer = null;
-        }
-
-        fadeTimer = new Timer(50, new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-
-                alpha += steps;
-
-                if (alpha >= 1.0) {
-                    alpha = 1.0f;
-                    if (fadeTimer != null) fadeTimer.stop();
-                }
-
-                LockPanel.this.repaint();
-
-            }
-        });
-        fadeTimer.setRepeats(true);
-        fadeTimer.setInitialDelay(0);
-        fadeTimer.start();
-
-    }
-
-    protected void paintComponent(Graphics g) {
-        Composite comp = ((Graphics2D) g).getComposite();
-        ((Graphics2D) g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
-
-        g.drawImage(screen, 0, 0, null);
-        ((Graphics2D) g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-        g.drawImage(gray, 0, 0, null);
-        ((Graphics2D) g).setComposite(comp);
-
-    }
-
-    private float alpha = 0.1f;
 
     /**
      * @return
      */
     private BufferedImage createScreenShot() {
         frame.toFront();
-        boolean top = frame.isAlwaysOnTop();
+        final boolean top = frame.isAlwaysOnTop();
         try {
             return new EDTHelper<BufferedImage>() {
                 @Override
                 public BufferedImage edtRun() {
                     if (frame.isShowing()) {
                         frame.setAlwaysOnTop(true);
-                        Rectangle captureSize = new Rectangle(frame.getContentPane().getSize());
-                        Point loc = frame.getContentPane().getLocationOnScreen();
+                        final Rectangle captureSize = new Rectangle(frame.getContentPane().getSize());
+                        final Point loc = frame.getContentPane().getLocationOnScreen();
                         captureSize.x = loc.x;
                         captureSize.y = loc.y;
 
@@ -276,24 +177,134 @@ public class LockPanel extends JPanel {
         }
     }
 
-    /**
-     * @param parentOwner
-     * @throws AWTException
-     */
-    public synchronized static LockPanel create(JFrame parentOwner) throws AWTException {
-        LockPanel ret = CACHE.get(parentOwner);
-        if (ret == null) {
-            ret = new LockPanel(parentOwner);
-            CACHE.put(parentOwner, ret);
+    public synchronized void fadeIn(final int time) {
+        fadeCounter++;
+        steps = 50 * 1.0 / time;
+        if (fadeTimer != null) {
+            fadeTimer.stop();
+            fadeTimer = null;
         }
-        return ret;
+
+        fadeTimer = new Timer(50, new ActionListener() {
+
+            public void actionPerformed(final ActionEvent e) {
+
+                alpha += steps;
+
+                if (alpha >= 1.0) {
+                    alpha = 1.0f;
+                    if (fadeTimer != null) {
+                        fadeTimer.stop();
+                    }
+                }
+
+                LockPanel.this.repaint();
+
+            }
+        });
+        fadeTimer.setRepeats(true);
+        fadeTimer.setInitialDelay(0);
+        fadeTimer.start();
+
+    }
+
+    public synchronized void fadeOut(final int time) {
+
+        screen = createScreenShot();
+        fadeCounter--;
+        steps = 50 * 1.0 / time;
+        if (fadeCounter > 0) { return; }
+        if (fadeTimer != null) {
+            fadeTimer.stop();
+            fadeTimer = null;
+        }
+
+        fadeTimer = new Timer(50, new ActionListener() {
+
+            public void actionPerformed(final ActionEvent e) {
+                alpha -= steps;
+                System.out.println(alpha);
+                if (alpha <= 0.0) {
+                    alpha = 0.0f;
+                    if (fadeTimer != null) {
+                        fadeTimer.stop();
+                    }
+                    fadeTimer = null;
+                    setWaitingPanelText(null);
+
+                    frame.getGlassPane().setVisible(false);
+                }
+
+                LockPanel.this.repaint();
+            }
+        });
+        fadeTimer.setRepeats(true);
+        fadeTimer.setInitialDelay(0);
+        fadeTimer.start();
+
+    }
+
+    /**
+     * @return the text
+     */
+    public JTextArea getText() {
+        return text;
+    }
+
+    /**
+     * @param time
+     */
+    public void lock(final int time) {
+        frame.getGlassPane().setVisible(false);
+        screen = createScreenShot();
+        frame.getGlassPane().setVisible(true);
+        if (screen != null) {
+            gray = ImageProvider.convertToGrayScale(screen);
+
+            final float data[] = { 0.0625f, 0.125f, 0.0625f, 0.125f, 0.25f, 0.125f, 0.0625f, 0.125f, 0.0625f };
+            final Kernel kernel = new Kernel(3, 3, data);
+            final ConvolveOp convolve = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
+            final BufferedImage dest = new BufferedImage(gray.getWidth(), gray.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            gray = convolve.filter(gray, dest);
+            frame.setGlassPane(this);
+
+            frame.getGlassPane().setVisible(true);
+
+            fadeIn(time);
+        }
+
+    }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        final Composite comp = ((Graphics2D) g).getComposite();
+        ((Graphics2D) g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+
+        g.drawImage(screen, 0, 0, null);
+        ((Graphics2D) g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+        g.drawImage(gray, 0, 0, null);
+        ((Graphics2D) g).setComposite(comp);
+
+    }
+
+    public void setWaitingPanelText(final String wait) {
+        if (wait == null) {
+            waitingPanel.setVisible(false);
+        } else {
+
+            text.setText(wait);
+            waitingPanel.pack();
+            waitingPanel.setLocation(SwingUtils.getCenter(frame, waitingPanel));
+            waitingPanel.setVisible(true);
+
+        }
 
     }
 
     /**
      * @param i
      */
-    public void unlock(int i) {
+    public void unlock(final int i) {
         fadeOut(i);
 
     }
