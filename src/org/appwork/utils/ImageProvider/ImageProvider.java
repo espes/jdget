b957a3356409ad103a328cc7b34cadbdea5b9b82
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2011-01-07 14:29:39 +0000

    Screenshot wizard
    git-svn-id: svn://svn.appwork.org/utils@424 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/ImageProvider/ImageProvider.java
+++ src/org/appwork/utils/ImageProvider/ImageProvider.java
@@ -16,6 +16,7 @@ import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -202,6 +203,81 @@ public class ImageProvider {
             ImageProvider.IMAGEICON_CACHE.put(key, imageicon);
             return imageicon;
         }
+    }
+
+    /**
+     * Taken from http://today.java.net/pub/a/today/2007/04/03/perils-of-image-
+     * getscaledinstance.html License: unknown Convenience method that returns a
+     * scaled instance of the provided {@code BufferedImage}.
+     * 
+     * @param img
+     *            the original image to be scaled
+     * @param targetWidth
+     *            the desired width of the scaled instance, in pixels
+     * @param targetHeight
+     *            the desired height of the scaled instance, in pixels
+     * @param hint
+     *            one of the rendering hints that corresponds to {@code
+     *            RenderingHints.KEY_INTERPOLATION} (e.g. {@code
+     *            RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR}, {@code
+     *            RenderingHints.VALUE_INTERPOLATION_BILINEAR}, {@code
+     *            RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality
+     *            if true, this method will use a multi-step scaling technique
+     *            that provides higher quality than the usual one-step technique
+     *            (only useful in downscaling cases, where {@code targetWidth}
+     *            or {@code targetHeight} is smaller than the original
+     *            dimensions, and generally only when the {@code BILINEAR} hint
+     *            is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     */
+    public static BufferedImage getScaledInstance(final BufferedImage img, int width, int height, final Object hint, final boolean higherQuality) {
+        final double faktor = Math.max((double) img.getWidth() / width, (double) img.getHeight() / height);
+        width = (int) (img.getWidth() / faktor);
+        height = (int) (img.getHeight() / faktor);
+        if (faktor == 1.0) { return img; }
+
+        final int type = img.getTransparency() == Transparency.OPAQUE ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = width;
+            h = height;
+        }
+
+        do {
+            if (higherQuality && w > width) {
+                w /= 2;
+                if (w < width) {
+                    w = width;
+                }
+            }
+
+            if (higherQuality && h > height) {
+                h /= 2;
+                if (h < height) {
+                    h = height;
+                }
+            }
+
+            final BufferedImage tmp = new BufferedImage(w, h, type);
+            final Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != width || h != height);
+
+        return ret;
     }
 
     /**
