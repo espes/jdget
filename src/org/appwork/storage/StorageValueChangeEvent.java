619346f4011f340ea55041fee07e64f7b3590674
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2011-01-17 12:59:17 +0000

    git-svn-id: svn://svn.appwork.org/utils@435 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/storage/StorageValueChangeEvent.java
+++ src/org/appwork/storage/StorageValueChangeEvent.java
@@ -41,4 +41,22 @@ public class StorageValueChangeEvent<E> extends StorageEvent<E> {
         return this.getParameter(0);
     }
 
+    /**
+     * TODO: check
+     * 
+     * @return
+     */
+    public boolean hasChanged() {
+        final E newV = getNewValue();
+        final E oldV = this.getOldValue();
+        if (oldV == null && newV != null) {
+            return true;
+        } else if (newV == null && oldV != null) {
+            return true;
+        } else if (newV != null) { return newV.equals(oldV);
+
+        }
+        return false;
+    }
+
 }
