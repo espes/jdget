677ba5cf448847980833dace125eb952c3578d7b
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2011-02-04 19:14:53 +0000

    git-svn-id: svn://svn.appwork.org/utils@512 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/net/SimpleHTTP.java
+++ src/org/appwork/utils/net/SimpleHTTP.java
@@ -12,10 +12,12 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.logging.Level;
 import java.util.zip.GZIPInputStream;
 
 import org.appwork.utils.Application;
 import org.appwork.utils.locale.Loc;
+import org.appwork.utils.logging.Log;
 
 public class SimpleHTTP {
 
@@ -25,12 +27,12 @@ public class SimpleHTTP {
     private HttpURLConnection             connection;
 
     public SimpleHTTP() {
-        requestHeader = new HashMap<String, String>();
+        this.requestHeader = new HashMap<String, String>();
 
     }
 
     public void clearRequestHeader() {
-        requestHeader.clear();
+        this.requestHeader.clear();
     }
 
     public byte[] download(final URL url, final DownloadProgress progress, final long maxSize) throws IOException, InterruptedException {
@@ -39,27 +41,31 @@ public class SimpleHTTP {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
 
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setInstanceFollowRedirects(true);
-            connection.setConnectTimeout(15000);
-            connection.setReadTimeout(30000);
-            final String loc = Loc.getLocale().split("_")[0];
-            connection.setRequestProperty("Accept-Language", loc);
-            connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
-            connection.setRequestProperty("Connection", "Close");
-            for (final Entry<String, String> next : requestHeader.entrySet()) {
-                connection.setRequestProperty(next.getKey(), next.getValue());
+            this.connection = (HttpURLConnection) url.openConnection();
+            this.connection.setInstanceFollowRedirects(true);
+            this.connection.setConnectTimeout(15000);
+            this.connection.setReadTimeout(30000);
+            try {
+                final String loc = Loc.getLocale().split("_")[0];
+                this.connection.setRequestProperty("Accept-Language", loc);
+            } catch (final Throwable e) {
+                Log.exception(Level.WARNING, e);
             }
-            connection.connect();
-            if (url.openConnection().getHeaderField("Content-Encoding") != null && connection.getHeaderField("Content-Encoding").equalsIgnoreCase("gzip")) {
-                input = new BufferedInputStream(gzi = new GZIPInputStream(connection.getInputStream()));
+            this.connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
+            this.connection.setRequestProperty("Connection", "Close");
+            for (final Entry<String, String> next : this.requestHeader.entrySet()) {
+                this.connection.setRequestProperty(next.getKey(), next.getValue());
+            }
+            this.connection.connect();
+            if (url.openConnection().getHeaderField("Content-Encoding") != null && this.connection.getHeaderField("Content-Encoding").equalsIgnoreCase("gzip")) {
+                input = new BufferedInputStream(gzi = new GZIPInputStream(this.connection.getInputStream()));
             } else {
-                input = new BufferedInputStream(connection.getInputStream());
+                input = new BufferedInputStream(this.connection.getInputStream());
             }
 
-            if (maxSize > 0 && connection.getContentLength() > maxSize) { throw new IOException("Max size exeeded!"); }
+            if (maxSize > 0 && this.connection.getContentLength() > maxSize) { throw new IOException("Max size exeeded!"); }
             if (progress != null) {
-                progress.setTotal(connection.getContentLength());
+                progress.setTotal(this.connection.getContentLength());
             }
             final byte[] b = new byte[32767];
             int len;
@@ -83,7 +89,7 @@ public class SimpleHTTP {
             } catch (final Exception e) {
             }
             try {
-                connection.disconnect();
+                this.connection.disconnect();
             } catch (final Throwable e) {
             }
 
@@ -92,7 +98,7 @@ public class SimpleHTTP {
     }
 
     public HttpURLConnection getConnection() {
-        return connection;
+        return this.connection;
     }
 
     public String getPage(final URL url) throws IOException, InterruptedException {
@@ -101,19 +107,25 @@ public class SimpleHTTP {
             InputStreamReader isr = null;
             try {
 
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setConnectTimeout(20000);
-                connection.setReadTimeout(20000);
-                connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
-                connection.setRequestProperty("Connection", "Close");
-                connection.setRequestProperty("Accept-Charset", "UTF-8");
-                for (final Entry<String, String> next : requestHeader.entrySet()) {
-                    connection.setRequestProperty(next.getKey(), next.getValue());
+                this.connection = (HttpURLConnection) url.openConnection();
+                this.connection.setConnectTimeout(20000);
+                this.connection.setReadTimeout(20000);
+                try {
+                    final String loc = Loc.getLocale().split("_")[0];
+                    this.connection.setRequestProperty("Accept-Language", loc);
+                } catch (final Throwable e) {
+                    Log.exception(Level.WARNING, e);
+                }
+                this.connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
+                this.connection.setRequestProperty("Connection", "Close");
+                this.connection.setRequestProperty("Accept-Charset", "UTF-8");
+                for (final Entry<String, String> next : this.requestHeader.entrySet()) {
+                    this.connection.setRequestProperty(next.getKey(), next.getValue());
                 }
                 int lookupTry = 0;
                 while (true) {
                     try {
-                        connection.connect();
+                        this.connection.connect();
                         break;
                     } catch (final UnknownHostException e) {
                         if (++lookupTry > 3) { throw e; }
@@ -122,7 +134,7 @@ public class SimpleHTTP {
                     }
                 }
 
-                in = new BufferedReader(isr = new InputStreamReader(connection.getInputStream(), "UTF-8"));
+                in = new BufferedReader(isr = new InputStreamReader(this.connection.getInputStream(), "UTF-8"));
 
                 String str;
                 final StringBuilder sb = new StringBuilder();
@@ -146,7 +158,7 @@ public class SimpleHTTP {
                 } catch (final Throwable e) {
                 }
                 try {
-                    connection.disconnect();
+                    this.connection.disconnect();
                 } catch (final Throwable e) {
                 }
 
@@ -155,13 +167,13 @@ public class SimpleHTTP {
     }
 
     public String getRequestHeader(final String key) {
-        return requestHeader.get(key);
+        return this.requestHeader.get(key);
     }
 
     public String getResponseHeader(final String string) {
         synchronized (SimpleHTTP.CALL_LOCK) {
-            if (connection == null) { return null; }
-            return connection.getHeaderField(string);
+            if (this.connection == null) { return null; }
+            return this.connection.getHeaderField(string);
 
         }
     }
@@ -170,20 +182,26 @@ public class SimpleHTTP {
         synchronized (SimpleHTTP.CALL_LOCK) {
             try {
 
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setConnectTimeout(20000);
+                this.connection = (HttpURLConnection) url.openConnection();
+                this.connection.setConnectTimeout(20000);
 
-                connection.setReadTimeout(readTimeout < 0 ? 20000 : readTimeout);
-                connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
-                connection.setRequestProperty("Connection", "Close");
-                connection.setRequestProperty("Accept-Charset", "UTF-8");
-                for (final Entry<String, String> next : requestHeader.entrySet()) {
-                    connection.setRequestProperty(next.getKey(), next.getValue());
+                this.connection.setReadTimeout(readTimeout < 0 ? 20000 : readTimeout);
+                try {
+                    final String loc = Loc.getLocale().split("_")[0];
+                    this.connection.setRequestProperty("Accept-Language", loc);
+                } catch (final Throwable e) {
+                    Log.exception(Level.WARNING, e);
+                }
+                this.connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
+                this.connection.setRequestProperty("Connection", "Close");
+                this.connection.setRequestProperty("Accept-Charset", "UTF-8");
+                for (final Entry<String, String> next : this.requestHeader.entrySet()) {
+                    this.connection.setRequestProperty(next.getKey(), next.getValue());
                 }
                 int lookupTry = 0;
                 while (true) {
                     try {
-                        connection.connect();
+                        this.connection.connect();
                         break;
                     } catch (final UnknownHostException e) {
                         if (++lookupTry > 3) { throw e; }
@@ -191,10 +209,10 @@ public class SimpleHTTP {
                         Thread.sleep(200);
                     }
                 }
-                return connection;
+                return this.connection;
             } finally {
                 try {
-                    connection.disconnect();
+                    this.connection.disconnect();
                 } catch (final Throwable e2) {
                 }
 
@@ -208,30 +226,35 @@ public class SimpleHTTP {
             OutputStream outputStream = null;
             try {
 
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setConnectTimeout(20000);
-                connection.setReadTimeout(20000);
-                connection.setRequestMethod("POST");
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                connection.setDoOutput(true);
-
-                connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
-                connection.setRequestProperty("Connection", "Close");
+                this.connection = (HttpURLConnection) url.openConnection();
+                this.connection.setConnectTimeout(20000);
+                this.connection.setReadTimeout(20000);
+                this.connection.setRequestMethod("POST");
+                this.connection.setDoInput(true);
+                this.connection.setUseCaches(false);
+                this.connection.setDoOutput(true);
+                try {
+                    final String loc = Loc.getLocale().split("_")[0];
+                    this.connection.setRequestProperty("Accept-Language", loc);
+                } catch (final Throwable e) {
+                    Log.exception(Level.WARNING, e);
+                }
+                this.connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
+                this.connection.setRequestProperty("Connection", "Close");
                 /* connection specific headers */
                 if (header != null) {
                     for (final Entry<String, String> next : header.entrySet()) {
-                        connection.setRequestProperty(next.getKey(), next.getValue());
+                        this.connection.setRequestProperty(next.getKey(), next.getValue());
                     }
                 }
-                for (final Entry<String, String> next : requestHeader.entrySet()) {
-                    connection.setRequestProperty(next.getKey(), next.getValue());
+                for (final Entry<String, String> next : this.requestHeader.entrySet()) {
+                    this.connection.setRequestProperty(next.getKey(), next.getValue());
                 }
 
                 int lookupTry = 0;
                 while (true) {
                     try {
-                        connection.connect();
+                        this.connection.connect();
                         break;
                     } catch (final UnknownHostException e) {
                         if (++lookupTry > 3) { throw e; }
@@ -239,16 +262,16 @@ public class SimpleHTTP {
                         Thread.sleep(200);
                     }
                 }
-                outputStream = connection.getOutputStream();
+                outputStream = this.connection.getOutputStream();
                 writer = new OutputStreamWriter(outputStream);
                 writer.write(postData);
                 writer.flush();
 
-                return connection;
+                return this.connection;
 
             } finally {
                 try {
-                    connection.disconnect();
+                    this.connection.disconnect();
                 } catch (final Throwable e2) {
                 }
                 try {
@@ -270,24 +293,29 @@ public class SimpleHTTP {
             OutputStream outputStream = null;
             InputStreamReader isr = null;
             try {
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setConnectTimeout(10000);
-                connection.setReadTimeout(10000);
-                connection.setRequestMethod("POST");
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                connection.setDoOutput(true);
-
-                connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
-                connection.setRequestProperty("Connection", "Close");
-                for (final Entry<String, String> next : requestHeader.entrySet()) {
-                    connection.setRequestProperty(next.getKey(), next.getValue());
+                this.connection = (HttpURLConnection) url.openConnection();
+                this.connection.setConnectTimeout(10000);
+                this.connection.setReadTimeout(10000);
+                this.connection.setRequestMethod("POST");
+                this.connection.setDoInput(true);
+                this.connection.setUseCaches(false);
+                this.connection.setDoOutput(true);
+                try {
+                    final String loc = Loc.getLocale().split("_")[0];
+                    this.connection.setRequestProperty("Accept-Language", loc);
+                } catch (final Throwable e) {
+                    Log.exception(Level.WARNING, e);
+                }
+                this.connection.setRequestProperty("User-Agent", "AppWork " + Application.getApplication());
+                this.connection.setRequestProperty("Connection", "Close");
+                for (final Entry<String, String> next : this.requestHeader.entrySet()) {
+                    this.connection.setRequestProperty(next.getKey(), next.getValue());
                 }
 
                 int lookupTry = 0;
                 while (true) {
                     try {
-                        connection.connect();
+                        this.connection.connect();
                         break;
                     } catch (final UnknownHostException e) {
                         if (++lookupTry > 3) { throw e; }
@@ -295,11 +323,11 @@ public class SimpleHTTP {
                         Thread.sleep(200);
                     }
                 }
-                outputStream = connection.getOutputStream();
+                outputStream = this.connection.getOutputStream();
                 writer = new OutputStreamWriter(outputStream);
                 writer.write(data);
                 writer.flush();
-                reader = new BufferedReader(isr = new InputStreamReader(connection.getInputStream(), "UTF-8"));
+                reader = new BufferedReader(isr = new InputStreamReader(this.connection.getInputStream(), "UTF-8"));
                 final StringBuilder sb = new StringBuilder();
                 String str;
                 while ((str = reader.readLine()) != null) {
@@ -330,7 +358,7 @@ public class SimpleHTTP {
                 } catch (final Throwable e) {
                 }
                 try {
-                    connection.disconnect();
+                    this.connection.disconnect();
                 } catch (final Throwable e) {
                 }
 
@@ -339,7 +367,7 @@ public class SimpleHTTP {
     }
 
     public void putRequestHeader(final String key, final String value) {
-        requestHeader.put(key, value);
+        this.requestHeader.put(key, value);
     }
 
 }
