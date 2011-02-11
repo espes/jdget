471fc3e9b7a649690571ea15d08f9f9815c7e729
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2011-02-11 15:54:00 +0000

    ClipboardUtils: only use absolute pathnames
    git-svn-id: svn://svn.appwork.org/utils@529 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/ClipboardUtils.java
+++ src/org/appwork/utils/ClipboardUtils.java
@@ -24,11 +24,11 @@ import org.appwork.utils.parser.HTMLParser;
 
 public class ClipboardUtils {
 
-    private static ClipboardUtils INSTANCE = new ClipboardUtils();
+    private static ClipboardUtils  INSTANCE       = new ClipboardUtils();
 
     public final static DataFlavor fileListFlavor = DataFlavor.javaFileListFlavor;
-    public final static DataFlavor stringFlavor = DataFlavor.stringFlavor;
-    private final static byte[] tmpByteArray = new byte[0];
+    public final static DataFlavor stringFlavor   = DataFlavor.stringFlavor;
+    private final static byte[]    tmpByteArray   = new byte[0];
     public final static DataFlavor arrayListFlavor;
     public final static DataFlavor uriListFlavor;
 
@@ -36,45 +36,61 @@ public class ClipboardUtils {
         DataFlavor tmp;
         try {
             tmp = new DataFlavor("text/uri-list; class=java.lang.String");
-        } catch (Throwable e) {
+        } catch (final Throwable e) {
             tmp = null;
         }
         uriListFlavor = tmp;
         try {
             tmp = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=java.util.ArrayList");
-        } catch (Throwable e) {
+        } catch (final Throwable e) {
             tmp = null;
         }
         arrayListFlavor = tmp;
     }
 
-    public static ClipboardUtils getInstance() {
-        return INSTANCE;
-    }
-
-    private ClipboardUtils() {
-    }
-
-    public static boolean hasSupport(DataFlavor flavor) {
-        if (flavor != null) {
-            if (uriListFlavor != null && flavor.isMimeTypeEqual(uriListFlavor)) return true;
-            if (fileListFlavor != null && flavor.isMimeTypeEqual(fileListFlavor)) return true;
-            if (arrayListFlavor != null && flavor.isMimeTypeEqual(arrayListFlavor)) return true;
-        }
-        return false;
-    }
-
-    public static boolean hasSupport(TransferSupport info) {
+    @SuppressWarnings("unchecked")
+    public static ArrayList<File> getFiles(final TransferSupport info) {
+        final ArrayList<File> files = new ArrayList<File>();
+        String inString = null;
         if (info != null) {
-            for (DataFlavor flavor : info.getDataFlavors()) {
-                if (hasSupport(flavor)) return true;
+            StringTokenizer izer;
+            try {
+                if (info.isDataFlavorSupported(ClipboardUtils.fileListFlavor)) {
+                    final List<File> list = (List<File>) info.getTransferable().getTransferData(ClipboardUtils.fileListFlavor);
+                    for (final File f : list) {
+                        if (f.isAbsolute() && f.exists()) {
+                            files.add(f);
+                        }
+                    }
+                } else if (ClipboardUtils.uriListFlavor != null && info.isDataFlavorSupported(ClipboardUtils.uriListFlavor)) {
+                    inString = (String) info.getTransferable().getTransferData(ClipboardUtils.uriListFlavor);
+                    izer = new StringTokenizer(inString, "\r\n");
+                    while (izer.hasMoreTokens()) {
+                        final String token = izer.nextToken().trim();
+                        try {
+                            final URI fi = new URI(token);
+                            final File f = new File(fi.getPath());
+                            if (f.isAbsolute() && f.exists()) {
+                                files.add(f);
+                            }
+                        } catch (final Throwable e) {
+                        }
+                    }
+                }
+            } catch (final Exception e) {
+                Log.L.warning(inString);
+                Log.L.warning(e.getMessage());
             }
         }
-        return false;
+        return files;
     }
 
-    public static ArrayList<String> getLinks(TransferSupport trans) {
-        ArrayList<String> links = new ArrayList<String>();
+    public static ClipboardUtils getInstance() {
+        return ClipboardUtils.INSTANCE;
+    }
+
+    public static ArrayList<String> getLinks(final TransferSupport trans) {
+        final ArrayList<String> links = new ArrayList<String>();
         String content = null;
         DataFlavor htmlFlavor = null;
         /*
@@ -82,8 +98,10 @@ public class ClipboardUtils {
          */
         try {
             for (final DataFlavor flav : trans.getTransferable().getTransferDataFlavors()) {
-                if (flav.getMimeType().contains("html") && flav.getRepresentationClass().isInstance(tmpByteArray)) {
-                    if (htmlFlavor != null) htmlFlavor = flav;
+                if (flav.getMimeType().contains("html") && flav.getRepresentationClass().isInstance(ClipboardUtils.tmpByteArray)) {
+                    if (htmlFlavor != null) {
+                        htmlFlavor = flav;
+                    }
                     final String charSet = new Regex(flav.toString(), "charset=(.*?)]").getMatch(0);
                     if (charSet != null && charSet.equalsIgnoreCase("UTF-8")) {
                         /* we found utf-8 encoding, so lets use that */
@@ -121,49 +139,37 @@ public class ClipboardUtils {
                 }
             } else {
                 /* try stringFlavor */
-                if (trans.isDataFlavorSupported(stringFlavor)) content = ((String) trans.getTransferable().getTransferData(stringFlavor));
+                if (trans.isDataFlavorSupported(ClipboardUtils.stringFlavor)) {
+                    content = (String) trans.getTransferable().getTransferData(ClipboardUtils.stringFlavor);
+                }
             }
             if (content != null) {
                 links.addAll(HTMLParser.findUrls(content));
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             Log.L.info(e.getMessage());
         }
         return links;
     }
 
-    @SuppressWarnings("unchecked")
-    public static ArrayList<File> getFiles(TransferSupport info) {
-        ArrayList<File> files = new ArrayList<File>();
-        String inString = null;
+    public static boolean hasSupport(final DataFlavor flavor) {
+        if (flavor != null) {
+            if (ClipboardUtils.uriListFlavor != null && flavor.isMimeTypeEqual(ClipboardUtils.uriListFlavor)) { return true; }
+            if (ClipboardUtils.fileListFlavor != null && flavor.isMimeTypeEqual(ClipboardUtils.fileListFlavor)) { return true; }
+            if (ClipboardUtils.arrayListFlavor != null && flavor.isMimeTypeEqual(ClipboardUtils.arrayListFlavor)) { return true; }
+        }
+        return false;
+    }
+
+    public static boolean hasSupport(final TransferSupport info) {
         if (info != null) {
-            StringTokenizer izer;
-            try {
-                if (info.isDataFlavorSupported(fileListFlavor)) {
-                    List<File> list = (List<File>) info.getTransferable().getTransferData(fileListFlavor);
-                    for (File f : list) {
-                        if (f.exists()) {
-                            files.add(f);
-                        }
-                    }
-                } else if (uriListFlavor != null && info.isDataFlavorSupported(uriListFlavor)) {
-                    inString = (String) info.getTransferable().getTransferData(uriListFlavor);
-                    izer = new StringTokenizer(inString, "\r\n");
-                    while (izer.hasMoreTokens()) {
-                        String token = izer.nextToken().trim();
-                        try {
-                            URI fi = new URI(token);
-                            File f = new File(fi.getPath());
-                            if (f.exists()) files.add(f);
-                        } catch (Throwable e) {
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                Log.L.warning(inString);
-                Log.L.warning(e.getMessage());
+            for (final DataFlavor flavor : info.getDataFlavors()) {
+                if (ClipboardUtils.hasSupport(flavor)) { return true; }
             }
         }
-        return files;
+        return false;
+    }
+
+    private ClipboardUtils() {
     }
 }
