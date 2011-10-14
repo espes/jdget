93a84a7873a2d8bf6046538a54ad44db380606db
thomas <thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2011-10-14 14:54:15 +0000

    New Pluginloader. Not finished yet - commit contains compile errors!
    git-svn-id: svn://svn.appwork.org/utils@953 21714237-3853-44ef-a1f0-ef8f03a7d1fe

---
+++ src/org/appwork/exceptions/TODOException.java
@@ -0,0 +1,49 @@
+package org.appwork.exceptions;
+
+public class TODOException extends RuntimeException {
+
+    /**
+     * 
+     */
+    public TODOException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace
+     */
+    public TODOException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public TODOException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public TODOException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public TODOException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
