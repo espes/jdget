460f228aec1f1bab3bb3fdb01b58c9329b4eee98
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-02-26 12:59:59 +0000

    Updated for Socks4(a) support
    git-svn-id: svn://svn.jdownloader.org/jdownloader@16017 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- browser/src/jd/http/HTTPConnectionFactory.java
+++ browser/src/jd/http/HTTPConnectionFactory.java
@@ -11,6 +11,7 @@ public class HTTPConnectionFactory {
         if (proxy.isNone()) { return new URLConnectionAdapterDirectImpl(url, proxy); }
         if (proxy.isDirect()) { return new URLConnectionAdapterDirectImpl(url, proxy); }
         if (proxy.getType().equals(HTTPProxy.TYPE.SOCKS5)) { return new URLConnectionAdapterSocks5Impl(url, proxy); }
+        if (proxy.getType().equals(HTTPProxy.TYPE.SOCKS4)) { return new URLConnectionAdapterSocks4Impl(url, proxy); }
         if (proxy.getType().equals(HTTPProxy.TYPE.HTTP)) { return new URLConnectionAdapterHTTPProxyImpl(url, proxy); }
         throw new RuntimeException("unsupported proxy type: " + proxy.getType().name());
     }
