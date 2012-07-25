61ed25dc1443e23b928399b7c1674dd4903d732c
Thomas <Thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2012-07-25 13:01:03 +0000

    Global IO Error Handler
    git-svn-id: svn://svn.appwork.org/utils@1340 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/IO.java
+++ src/org/appwork/utils/IO.java
@@ -27,55 +27,69 @@ import org.appwork.utils.os.CrossSystem;
 
 public class IO {
     public static void copyFile(final File in, final File out) throws IOException {
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
-        FileChannel inChannel = null;
-        FileChannel outChannel = null;
-        if (Log.L.isLoggable(Level.FINEST)) {
-            Log.L.finest("Copy " + in + " to " + out);
-        }
         try {
-            inChannel = (fis = new FileInputStream(in)).getChannel();
-            outChannel = (fos = new FileOutputStream(out)).getChannel();
-            if (CrossSystem.isWindows()) {
-                // magic number for Windows, 64Mb - 32Kb)
-                // On the Windows plateform, you can't copy a file bigger than
-                // 64Mb,
-                // an Exception in thread "main" java.io.IOException:
-                // Insufficient
-                // system resources exist to complete the requested service is
-                // thrown.
-                //
-                // For a discussion about this see :
-                // http://forum.java.sun.com/thread.jspa?threadID=439695&messageID=2917510
-                final int maxCount = 64 * 1024 * 1024 - 32 * 1024;
-                final long size = inChannel.size();
-                long position = 0;
-                while (position < size) {
-                    position += inChannel.transferTo(position, maxCount, outChannel);
+            FileInputStream fis = null;
+            FileOutputStream fos = null;
+            FileChannel inChannel = null;
+            FileChannel outChannel = null;
+            if (Log.L.isLoggable(Level.FINEST)) {
+                Log.L.finest("Copy " + in + " to " + out);
+            }
+            try {
+                inChannel = (fis = new FileInputStream(in)).getChannel();
+                outChannel = (fos = new FileOutputStream(out)).getChannel();
+                if (CrossSystem.isWindows()) {
+                    // magic number for Windows, 64Mb - 32Kb)
+                    // On the Windows plateform, you can't copy a file bigger
+                    // than
+                    // 64Mb,
+                    // an Exception in thread "main" java.io.IOException:
+                    // Insufficient
+                    // system resources exist to complete the requested service
+                    // is
+                    // thrown.
+                    //
+                    // For a discussion about this see :
+                    // http://forum.java.sun.com/thread.jspa?threadID=439695&messageID=2917510
+                    final int maxCount = 64 * 1024 * 1024 - 32 * 1024;
+                    final long size = inChannel.size();
+                    long position = 0;
+                    while (position < size) {
+                        position += inChannel.transferTo(position, maxCount, outChannel);
+                    }
+                } else {
+                    inChannel.transferTo(0, inChannel.size(), outChannel);
                 }
-            } else {
-                inChannel.transferTo(0, inChannel.size(), outChannel);
+            } catch (final IOException e) {
+                throw e;
+            } finally {
+                try {
+                    outChannel.close();
+                } catch (final Throwable e) {
+                }
+                try {
+                    fos.close();
+                } catch (final Throwable e) {
+                }
+                try {
+                    inChannel.close();
+                } catch (final Throwable e) {
+                }
+                try {
+                    fis.close();
+                } catch (final Throwable e) {
+                }
             }
-        } catch (final IOException e) {
+        } catch (IOException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onCopyException(e, in, out);
+
             throw e;
-        } finally {
-            try {
-                outChannel.close();
-            } catch (final Throwable e) {
-            }
-            try {
-                fos.close();
-            } catch (final Throwable e) {
-            }
-            try {
-                inChannel.close();
-            } catch (final Throwable e) {
-            }
-            try {
-                fis.close();
-            } catch (final Throwable e) {
-            }
+        } catch (RuntimeException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onCopyException(e, in, out);
+            throw e;
+        } catch (Error e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onCopyException(e, in, out);
+            throw e;
         }
     }
 
@@ -141,6 +155,16 @@ public class IO {
             }
 
             return ret.toString();
+        } catch (IOException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, fis);
+
+            throw e;
+        } catch (RuntimeException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, fis);
+            throw e;
+        } catch (Error e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, fis);
+            throw e;
         } finally {
             try {
                 f.close();
@@ -156,6 +180,7 @@ public class IO {
      * 
      * this function skips emtpy lines
      */
+
     public static String readLine(final BufferedInputStream is, final byte[] array) throws IOException {
         Arrays.fill(array, 0, array.length, (byte) 0);
         int read = 0;
@@ -201,6 +226,17 @@ public class IO {
                     if (maxSize > 0 && baos.size() > maxSize) { throw new IOException("Max size exeeded!"); }
                 }
             }
+
+        } catch (IOException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, input);
+
+            throw e;
+        } catch (RuntimeException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, input);
+            throw e;
+        } catch (Error e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onReadStreamException(e, input);
+            throw e;
         } finally {
             try {
                 ReusableByteArrayOutputStreamPool.reuseReusableByteArrayOutputStream(os);
@@ -274,59 +310,99 @@ public class IO {
     }
 
     public static void writeStringToFile(final File file, final String string) throws IOException {
-        if (file == null) { throw new IllegalArgumentException("File is null."); }
-        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
-        file.createNewFile();
-        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
-        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
-        FileWriter fw = null;
-        if (Log.L.isLoggable(Level.FINEST)) {
-            Log.L.finest("Write " + file);
-        }
-        
-        final Writer output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"UTF-8"));
-
         try {
-            output.write(string);
-        } finally {
-            try {
-                output.flush();
-            } catch (final Throwable e) {
+            if (file == null) { throw new IllegalArgumentException("File is null."); }
+            if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
+            file.createNewFile();
+            if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
+            if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
+            FileWriter fw = null;
+            if (Log.L.isLoggable(Level.FINEST)) {
+                Log.L.finest("Write " + file);
             }
+
+            final Writer output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+
             try {
-                fw.flush();
-            } catch (final Throwable e) {
+                output.write(string);
+            } finally {
+                try {
+                    output.flush();
+                } catch (final Throwable e) {
+                }
+                try {
+                    fw.flush();
+                } catch (final Throwable e) {
+                }
+                try {
+                    output.close();
+                } catch (final Throwable e) {
+                }
+                try {
+                    fw.close();
+                } catch (final Throwable e) {
+                }
             }
-            try {
-                output.close();
-            } catch (final Throwable e) {
-            }
-            try {
-                fw.close();
-            } catch (final Throwable e) {
-            }
+        } catch (IOException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, string.getBytes());
+
+            throw e;
+        } catch (RuntimeException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, string.getBytes());
+            throw e;
+        } catch (Error e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, string.getBytes());
+            throw e;
         }
 
+    }
+
+    private static IOErrorHandler ERROR_HANDLER = null;
+
+    public static IOErrorHandler getErrorHandler() {
+        return ERROR_HANDLER;
+    }
+
+    /**
+     * Want to get informed in case of any io problems, set this handler
+     * 
+     * @param handler
+     */
+    public static void setErrorHandler(IOErrorHandler handler) {
+
+        ERROR_HANDLER = handler;
     }
 
     public static void writeToFile(final File file, final byte[] data) throws IOException {
-        if (file == null) { throw new IllegalArgumentException("File is null."); }
-        if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
-        file.createNewFile();
-        if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
-        if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
-        if (Log.L.isLoggable(Level.FINEST)) {
-            Log.L.finest("Write " + file);
-        }
-        final FileOutputStream out = new FileOutputStream(file);
         try {
-            out.write(data);
-        } finally {
-            try {
-                out.close();
-            } catch (final Throwable e) {
+            if (file == null) { throw new IllegalArgumentException("File is null."); }
+            if (file.exists()) { throw new IllegalArgumentException("File already exists: " + file); }
+            file.createNewFile();
+            if (!file.isFile()) { throw new IllegalArgumentException("Is not a file: " + file); }
+            if (!file.canWrite()) { throw new IllegalArgumentException("Cannot write to file: " + file); }
+            if (Log.L.isLoggable(Level.FINEST)) {
+                Log.L.finest("Write " + file);
             }
+            final FileOutputStream out = new FileOutputStream(file);
+            try {
+
+                out.write(data);
+            } finally {
+                try {
+                    out.close();
+                } catch (final Throwable e) {
+                }
+            }
+        } catch (IOException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, data);
+
+            throw e;
+        } catch (RuntimeException e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, data);
+            throw e;
+        } catch (Error e) {
+            if (ERROR_HANDLER != null) ERROR_HANDLER.onWriteException(e, file, data);
+            throw e;
         }
     }
-
 }
