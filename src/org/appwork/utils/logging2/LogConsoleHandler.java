287985c8cbb84d6c4fd17eac131a47078aab30a7
Thomas <Thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-03-01 10:36:51 +0000

    build>full
    git-svn-id: svn://svn.appwork.org/utils@1563 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/logging2/LogConsoleHandler.java
+++ src/org/appwork/utils/logging2/LogConsoleHandler.java
@@ -9,7 +9,8 @@
  */
 package org.appwork.utils.logging2;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Filter;
 import java.util.logging.Level;
@@ -23,30 +24,38 @@ import org.appwork.utils.logging.LogFormatter;
  */
 public class LogConsoleHandler extends ConsoleHandler {
 
-    private ArrayList<String> allowedLoggerNames = new ArrayList<String>();
+    private HashSet<String> allowedLoggerNames = new HashSet<String>();
 
     public LogConsoleHandler() {
-        this.setLevel(Level.ALL);
-        this.setFormatter(new LogFormatter());
+        setLevel(Level.ALL);
+        setFormatter(new LogFormatter());
     }
 
-    public ArrayList<String> getAllowedLoggerNames() {
-        return this.allowedLoggerNames;
+    public HashSet<String> getAllowedLoggerNames() {
+        return allowedLoggerNames;
     }
 
     @Override
     public boolean isLoggable(final LogRecord record) {
-        final ArrayList<String> lallowedLoggerNames = this.allowedLoggerNames;
+        final HashSet<String> lallowedLoggerNames = allowedLoggerNames;
         if (lallowedLoggerNames == null) { return false; }
         if (lallowedLoggerNames.size() == 0) { return true; }
-        for (final String allowedLoggerName : lallowedLoggerNames) {
-            if (allowedLoggerName != null && allowedLoggerName.equalsIgnoreCase(record.getLoggerName())) { return true; }
-        }
+        if (allowedLoggerNames.contains(record.getLoggerName().toLowerCase(Locale.ENGLISH))) { return true; }
+
         return false;
     }
 
-    public void setAllowedLoggerNames(final ArrayList<String> allowedLoggerNames) {
-        this.allowedLoggerNames = allowedLoggerNames;
+    public void setAllowedLoggerNames(final String... strings) {
+        if (strings == null) {
+            allowedLoggerNames = null;
+            return;
+        } else {
+            final HashSet<String> tmp = new HashSet<String>();
+            for (final String s : strings) {
+                tmp.add(s.toLowerCase(Locale.ENGLISH));
+            }
+            allowedLoggerNames = tmp;
+        }
     }
 
     @Override
