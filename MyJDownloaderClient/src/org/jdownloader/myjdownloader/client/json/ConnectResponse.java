585ec1846bc3cd802799dc90efc973b29142c407
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-05 13:03:54 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@20404 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/ConnectResponse.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/ConnectResponse.java
@@ -2,23 +2,23 @@ package org.jdownloader.myjdownloader.client.json;
 
 public class ConnectResponse implements RequestIDValidator {
 
-    private String sessiontoken       = null;
-    private String regaintoken = null;
-    private long   rid   = -1;
+    private String sessiontoken = null;
+    private String regaintoken  = "blablablb";
+    private long   rid          = -1;
 
     public ConnectResponse(/* Storable */) {
     }
 
     public String getRegaintoken() {
-        return regaintoken;
+        return this.regaintoken;
     }
 
     public long getRid() {
-        return rid;
+        return this.rid;
     }
 
     public String getSessiontoken() {
-        return sessiontoken;
+        return this.sessiontoken;
     }
 
     public void setRegaintoken(final String regaintoken) {
@@ -26,7 +26,7 @@ public class ConnectResponse implements RequestIDValidator {
     }
 
     public void setRid(final long timestamp) {
-        rid = timestamp;
+        this.rid = timestamp;
     }
 
     public void setSessiontoken(final String token) {
