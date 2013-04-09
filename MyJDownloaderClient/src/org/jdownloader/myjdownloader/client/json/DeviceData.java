2043eb0e5d7d2c6cf1b334aa830c7c780a7408f8
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-09 09:32:50 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@20442 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/DeviceData.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/DeviceData.java
@@ -1,46 +1,64 @@
 package org.jdownloader.myjdownloader.client.json;
 
 public class DeviceData {
+    private String id;
+
+    private String type;
+    private long   lastSeenTimestamp = -1;
+    private String name;
+
     public DeviceData(/* storable */) {
 
     }
 
-    private String id;
-    private String type;
+    public DeviceData(final String deviceid, final String deviceType, final String deviceName) {
+        this.id = deviceid;
+        this.type = deviceType;
+        this.name = deviceName;
+    }
 
     public String getId() {
-        return id;
+        return this.id;
+    }
+
+    /**
+     * @return the lastSeenTimestamp
+     */
+    public long getLastSeenTimestamp() {
+        return this.lastSeenTimestamp;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 
     public void setId(final String id) {
         this.id = id;
     }
-    public String toString(){
-        return name+" ("+id+")";
-    }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(final String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
+    /**
+     * @param lastSeenTimestamp
+     *            the lastSeenTimestamp to set
+     */
+    public void setLastSeenTimestamp(final long lastSeenTimestamp) {
+        this.lastSeenTimestamp = lastSeenTimestamp;
     }
 
     public void setName(final String name) {
         this.name = name;
     }
 
-    private String name;
+    public void setType(final String type) {
+        this.type = type;
+    }
 
-    public DeviceData(final String deviceid, final String deviceType, final String deviceName) {
-        id = deviceid;
-        type = deviceType;
-        name = deviceName;
+    @Override
+    public String toString() {
+        return this.name + " (" + this.id + ")";
     }
 
 }
