4d7e6f069a035ecee19ba0cb13ea74c81308c0dc
wnickl <wnickl@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-18 17:57:59 +0000

    cleanup (minor refactoring): - formatted code - added javadoc - sorted all members
    git-svn-id: svn://svn.jdownloader.org/jdownloader@20538 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- browser/src/jd/http/requests/FormData.java
+++ browser/src/jd/http/requests/FormData.java
@@ -18,77 +18,177 @@ package jd.http.requests;
 
 import java.io.File;
 
+/**
+ * In many cases, the purpose of an HTML Form is to send data to a server. The server will process the data and then send a response to the
+ * user. Files are a special case with HTML forms. They are binary data—or considered as such—where all others data are text data. Because
+ * HTTP is a text protocol, there are special requirements to handle binary data. This class is simple POJO holding {@link File} reference,
+ * byte[] of data, a type and a name.
+ */
 public class FormData {
 
-    private final Type type;
-    private String name;
-    private final String value;
-
-    public String getName() {
-        return name;
+    /**
+     * enumeration of FormData types
+     */
+    public static enum Type {
+        DATA,
+        FILE,
+        VARIABLE
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+    /** default MIME type: {@value #DEFAULT_MIME} */
+    private static final String DEFAULT_MIME = "application/octet-stream";
 
-    public String getValue() {
-        return value;
-    }
+    /** data byte array */
+    private byte[]              data;
 
-    public byte[] getData() {
-        return data;
-    }
+    /** file */
+    private File                file;
 
-    public File getFile() {
-        return file;
-    }
+    /** the Multipurpose Internet Mail Extensions */
+    private String              mime;
 
-    private byte[] data;
-    private File file;
-    private String mime;
+    /** The name. */
+    private String              name;
 
+    /** The type. */
+    private final Type          type;
+
+    /** The value. */
+    private final String        value;
+
+    /**
+     * constructor
+     * 
+     * @param name
+     *            the name
+     * @param value
+     *            the value
+     */
     public FormData(final String name, final String value) {
         this.type = Type.VARIABLE;
         this.name = name;
         this.value = value;
     }
 
+    /**
+     * constructor
+     * 
+     * @param name
+     *            the name
+     * @param filename
+     *            the filename
+     * @param data
+     *            the data
+     */
     public FormData(final String name, final String filename, final byte[] data) {
         this(name, filename, null, data);
     }
 
+    /**
+     * constructor
+     * 
+     * @param name
+     *            the name
+     * @param filename
+     *            the filename
+     * @param file
+     *            the file
+     */
+    public FormData(final String name, final String filename, final File file) {
+        this(name, filename, null, file);
+
+    }
+
+    /**
+     * constructor
+     * 
+     * @param name
+     *            the name
+     * @param filename
+     *            the filename
+     * @param mime
+     *            the mime
+     * @param data
+     *            the data
+     */
     public FormData(final String name, final String filename, final String mime, final byte[] data) {
-        this.mime = (mime == null) ? "application/octet-stream" : mime;
+        this.mime = mime == null ? DEFAULT_MIME : mime;
         this.type = Type.DATA;
         this.name = name;
         this.value = filename;
         this.data = data;
     }
 
-    public FormData(final String name, final String filename, final File file) {
-        this(name, filename, null, file);
-
-    }
-
+    /**
+     * constructor
+     * 
+     * @param name
+     *            the name
+     * @param filename
+     *            the filename
+     * @param mime
+     *            the mime
+     * @param file
+     *            the file
+     */
     public FormData(final String name, final String filename, final String mime, final File file) {
-        this.mime = (mime == null) ? "application/octet-stream" : mime;
+        this.mime = mime == null ? DEFAULT_MIME : mime;
         this.type = Type.FILE;
         this.name = name;
         this.value = filename;
         this.file = file;
     }
 
-    public static enum Type {
-        VARIABLE, FILE, DATA
+    /**
+     * @return the data byte[]
+     */
+    public byte[] getData() {
+        return data;
     }
 
+    /**
+     * @return the mime data type
+     */
+    public String getDataType() {
+        return this.mime;
+    }
+
+    /**
+     * @return the file
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the type
+     */
     public Type getType() {
         return type;
     }
 
-    public String getDataType() {
-        return this.mime;
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the name.
+     * 
+     * @param name
+     *            the new name
+     */
+    public void setName(final String name) {
+        this.name = name;
     }
 
 }
