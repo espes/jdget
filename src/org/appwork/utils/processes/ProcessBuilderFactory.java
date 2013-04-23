ecfdccccc7b68542a77ed1ea787428411edca9db
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-04-23 12:08:26 +0000

    git-svn-id: svn://svn.appwork.org/utils@1628 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/processes/ProcessBuilderFactory.java
+++ src/org/appwork/utils/processes/ProcessBuilderFactory.java
@@ -1,37 +1,39 @@
 package org.appwork.utils.processes;
 
-import java.util.ArrayList;
-
 import org.appwork.utils.os.CrossSystem;
 
 public class ProcessBuilderFactory {
 
-    public static ProcessBuilder create(String ... tiny) {
+    public static ProcessBuilder create(final java.util.List<String> splitCommandString) {
+        return ProcessBuilderFactory.create(splitCommandString.toArray(new String[] {}));
 
-        return new ProcessBuilder(escape(tiny));
     }
 
-    private static String[] escape(String[] tiny) {
-        if (!CrossSystem.isWindows()) return tiny;
+    public static ProcessBuilder create(final String... tiny) {
 
-        // The windows processbuilder throws exceptions if a arguments starts with ", but does not end with " or vice versa
-        String[] ret = new String[tiny.length];
-        //
-        for (int i = 0; i < ret.length; i++) {
-            if (tiny[i].startsWith("\"") && !tiny[i].endsWith("\"")) {
-                ret[i] = "\"" + tiny[i].replace("\"", "\\\"") + "\"";
-            } else if (!tiny[i].startsWith("\"") && tiny[i].endsWith("\"")) {
-                ret[i] = "\"" + tiny[i].replace("\"", "\\\"") + "\"";
-            } else {
-                ret[i] = tiny[i];
+        return new ProcessBuilder(ProcessBuilderFactory.escape(tiny));
+    }
+
+    private static String[] escape(final String[] tiny) {
+        if (CrossSystem.isWindows() || CrossSystem.isOS2()) {
+            // The windows processbuilder throws exceptions if a arguments
+            // starts
+            // with ", but does not end with " or vice versa
+            final String[] ret = new String[tiny.length];
+            //
+            for (int i = 0; i < ret.length; i++) {
+                if (tiny[i].startsWith("\"") && !tiny[i].endsWith("\"")) {
+                    ret[i] = "\"" + tiny[i].replace("\"", "\\\"") + "\"";
+                } else if (!tiny[i].startsWith("\"") && tiny[i].endsWith("\"")) {
+                    ret[i] = "\"" + tiny[i].replace("\"", "\\\"") + "\"";
+                } else {
+                    ret[i] = tiny[i];
+                }
             }
+
+            return ret;
+        } else {
+            return tiny;
         }
-
-        return ret;
-    }
-
-    public static ProcessBuilder create(java.util.List<String> splitCommandString) {
-        return create(splitCommandString.toArray(new String[] {}));
-
     }
 }
