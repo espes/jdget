d7050aaa38f1d1547e5978b2b4202ff09d225ffc
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-04-25 08:03:28 +0000

    git-svn-id: svn://svn.appwork.org/utils@1633 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/event/AWTEventQueueLinker.java
+++ src/org/appwork/swing/event/AWTEventQueueLinker.java
@@ -4,6 +4,9 @@ import java.awt.AWTEvent;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 
+import org.appwork.utils.Application;
+import org.appwork.utils.swing.EDTRunner;
+
 /**
  * This class creates a copy paste cut select delete contecxtmenu for every
  * textcomponent.
@@ -14,44 +17,58 @@ public class AWTEventQueueLinker extends EventQueue {
 
     private static AWTEventQueueLinker INSTANCE;
 
-    public synchronized static void link() {
-        if (INSTANCE != null) {
-            return;
-        }
-        INSTANCE = new AWTEventQueueLinker();
-
-    }
-
-    public static boolean isLinked() {
-        return INSTANCE != null;
-    }
-
-    private AWTDispatchEventSender eventSender;
-
-    private AWTEventQueueLinker() {
-        super();
-        eventSender = new AWTDispatchEventSender();
-        Toolkit.getDefaultToolkit().getSystemEventQueue().push(this);
-
-    }
-
-    @Override
-    protected void dispatchEvent(final AWTEvent event) {
-        eventSender.fireEvent(new AWTDispatchEvent(this, AWTDispatchEvent.Type.PRE_DISPATCH, event));
-        super.dispatchEvent(event);
-        eventSender.fireEvent(new AWTDispatchEvent(this, AWTDispatchEvent.Type.POST_DISPATCH, event));
-
-    }
-
-    public AWTDispatchEventSender getEventSender() {
-        return eventSender;
-    }
-
     /**
      * @return
      */
     public static AWTEventQueueLinker getInstance() {
         // TODO Auto-generated method stub
-        return INSTANCE;
+        return AWTEventQueueLinker.INSTANCE;
+    }
+
+    public static boolean isLinked() {
+        return AWTEventQueueLinker.INSTANCE != null;
+    }
+
+    public synchronized static void link() {
+        if (AWTEventQueueLinker.INSTANCE != null) { return; }
+        AWTEventQueueLinker.INSTANCE = new AWTEventQueueLinker();
+    }
+
+    private final AWTDispatchEventSender eventSender;
+
+    private AWTEventQueueLinker() {
+        super();
+        this.eventSender = new AWTDispatchEventSender();
+        /* http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7097333 */
+        if (Application.getJavaVersion() >= Application.JAVA17) {
+            new EDTRunner() {
+
+                @Override
+                protected void runInEDT() {
+                    /* under JDK1.7 we have to push EventQueue inside EDT */
+                    Toolkit.getDefaultToolkit().getSystemEventQueue().push(AWTEventQueueLinker.this);
+                }
+            };
+        } else {
+            new Thread("EventQueuePushWorkaround") {
+                @Override
+                public void run() {
+                    /* under java <=1.6 we have to push EventQueue outside EDT */
+                    Toolkit.getDefaultToolkit().getSystemEventQueue().push(AWTEventQueueLinker.this);
+                };
+            }.start();
+        }
+    }
+
+    @Override
+    protected void dispatchEvent(final AWTEvent event) {
+        this.eventSender.fireEvent(new AWTDispatchEvent(this, AWTDispatchEvent.Type.PRE_DISPATCH, event));
+        super.dispatchEvent(event);
+        this.eventSender.fireEvent(new AWTDispatchEvent(this, AWTDispatchEvent.Type.POST_DISPATCH, event));
+
+    }
+
+    public AWTDispatchEventSender getEventSender() {
+        return this.eventSender;
     }
 }
