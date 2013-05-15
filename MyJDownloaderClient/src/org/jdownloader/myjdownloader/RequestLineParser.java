ed149d9a08244991314ad842311cd85cde11e163
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-05-15 15:47:21 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@20828 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/RequestLineParser.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/RequestLineParser.java
@@ -11,29 +11,42 @@ public class RequestLineParser {
                     if (array[i + 3] != '_') { return null; }
                     if (array[i + 44] != '_') { return null; }
                     final String sessionToken = new String(array, i + 4, 40, "ISO-8859-1");
-                    if (array[i + 77] != '/') { return new RequestLineParser(null, sessionToken); }
+                    if (array[i + 77] != '/') { return new RequestLineParser(null, sessionToken, null); }
                     final String deviceID = new String(array, i + 45, 32, "ISO-8859-1");
-                    return new RequestLineParser(deviceID, sessionToken);
+                    final int x = i + 45 + 32;
+                    for (i = x; i < array.length; i++) {
+                        if (array[i] == ' ' || array[i] == '?') {
+                            final String requestURL = new String(array, x, i - x, "ISO-8859-1");
+                            return new RequestLineParser(deviceID, sessionToken, requestURL);
+                        }
+                    }
+                    return new RequestLineParser(deviceID, sessionToken, null);
                 }
             }
         } catch (final Exception e) {
             e.printStackTrace();
         }
         return null;
-
     }
 
     private final String deviceID;
 
     private final String sessionToken;
 
-    private RequestLineParser(final String deviceID, final String sessionToken) {
+    private final String requestURL;
+
+    private RequestLineParser(final String deviceID, final String sessionToken, final String requestURL) {
         this.deviceID = deviceID;
         this.sessionToken = sessionToken;
+        this.requestURL = requestURL;
     }
 
     public String getDeviceID() {
         return this.deviceID;
+    }
+
+    public String getRequestURL() {
+        return this.requestURL;
     }
 
     public String getSessionToken() {
