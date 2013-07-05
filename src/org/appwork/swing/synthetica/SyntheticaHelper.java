4128ad4b3219bebde3ae3c1befbfdc850b1b0775
Thomas <Thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-07-05 10:06:19 +0000

    git-svn-id: svn://svn.appwork.org/utils@1741 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/synthetica/SyntheticaHelper.java
+++ src/org/appwork/swing/synthetica/SyntheticaHelper.java
@@ -1,20 +1,22 @@
 package org.appwork.swing.synthetica;
 
 import java.awt.Font;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
+import org.appwork.exceptions.WTFException;
 import org.appwork.storage.config.JsonConfig;
 import org.appwork.swing.components.ExtPasswordField;
 import org.appwork.txtresource.TranslationFactory;
 import org.appwork.utils.Application;
 import org.appwork.utils.IO;
 import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
 import org.appwork.utils.logging.Log;
 import org.appwork.utils.os.CrossSystem;
 
@@ -71,22 +73,73 @@ public class SyntheticaHelper {
     }
 
     /**
-     * @throws UnsupportedEncodingException
+     * @throws IOException
      * 
      */
-    public static void init() throws UnsupportedEncodingException {
+    public static void init() throws IOException {
         SyntheticaHelper.init("de.javasoft.plaf.synthetica.SyntheticaSimple2DLookAndFeel");
 
     }
 
+    public static void init(final String laf) throws IOException {
+
+        init(laf, readLicense());
+    }
+
+    /**
+     * @return
+     * @throws IOException 
+     */
+    private static String readLicense() throws IOException {
+        final URL url = Application.getRessourceURL("cfg/synthetica-license.key");
+        if (url == null) {
+
+            Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
+
+            Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
+
+            Log.L.warning("Missing Look And Feel License.");
+            Log.L.warning("You can only use Synthetica Look and Feel in official JDownloader versions.");
+            Log.L.warning("Reverted to your System Look And Feel!");
+            Log.L.warning("If you are a developer, and want to do some gui work on the offical JDownloader Look And Feel, write e-mail@appwork.org to get a developer Look And Feel Key");
+            throw new WTFException("No Synthetica License Found!");
+        }
+        return IO.readURLToString(url);
+    }
+
     /**
      * @param string
-     * @throws UnsupportedEncodingException
+     * @throws IOException
      */
-    public static void init(final String laf) throws UnsupportedEncodingException {
+    public static void init(final String laf, String license) throws IOException {
+        if (StringUtils.isEmpty(license)) {
+            license = readLicense();
+        }
         Log.L.info("LaF init: " + laf);
         final long start = System.currentTimeMillis();
         try {
+            /* we save around x-400 ms here by not using AES */
+            if (license == null) {
+
+                Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
+
+                Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
+
+                Log.L.warning("Missing Look And Feel License.");
+                Log.L.warning("You can only use Synthetica Look and Feel in official JDownloader versions.");
+                Log.L.warning("Reverted to your System Look And Feel!");
+                Log.L.warning("If you are a developer, and want to do some gui work on the offical JDownloader Look And Feel, write e-mail@appwork.org to get a developer Look And Feel Key");
+                throw new WTFException("No Synthetica License Found!");
+            }
+            String[] licenseLines = Regex.getLines(license);
+            // final String[] li = { };
+            final ArrayList<String> valids = new ArrayList<String>();
+            for (final String s : licenseLines) {
+                if (!s.trim().startsWith("#") && !s.trim().startsWith("//")) {
+                    valids.add(s);
+                }
+            }
+
             JFrame.setDefaultLookAndFeelDecorated(false);
 
             JDialog.setDefaultLookAndFeelDecorated(false);
@@ -127,19 +180,10 @@ public class SyntheticaHelper {
              * Version of synthetica found on javasoft.de
              */
 
-            /* we save around x-400 ms here by not using AES */
-            String[] license = Regex.getLines(IO.readURLToString(Application.getRessourceURL("cfg/synthetica-license.key")));
-            // final String[] li = { };
-            final ArrayList<String> valids = new ArrayList<String>();
-            for (final String s : license) {
-                if (!s.trim().startsWith("#") && !s.trim().startsWith("//")) {
-                    valids.add(s);
-                }
-            }
-            license = valids.toArray(new String[] {});
-            final String key = license[0];
-            final String[] li = new String[license.length - 1];
-            System.arraycopy(license, 1, li, 0, li.length);
+            licenseLines = valids.toArray(new String[] {});
+            final String key = licenseLines[0];
+            final String[] li = new String[licenseLines.length - 1];
+            System.arraycopy(licenseLines, 1, li, 0, li.length);
             if (key != null) {
                 UIManager.put("Synthetica.license.info", li);
                 UIManager.put("Synthetica.license.key", key);
@@ -162,33 +206,6 @@ public class SyntheticaHelper {
             }
 
             ExtPasswordField.MASK = "*******";
-
-        } catch (final Exception e) {
-            Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
-
-            Log.L.warning("Missing Look And Feel License. Reverted to your System Look And Feel!");
-
-            Log.L.warning("Missing Look And Feel License.");
-            Log.L.warning("You can only use Synthetica Look and Feel in official JDownloader versions.");
-            Log.L.warning("Reverted to your System Look And Feel!");
-            Log.L.warning("If you are a developer, and want to do some gui work on the offical JDownloader Look And Feel, write e-mail@appwork.org to get a developer Look And Feel Key");
-
-            Log.exception(e);
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (final ClassNotFoundException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } catch (final InstantiationException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } catch (final IllegalAccessException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } catch (final UnsupportedLookAndFeelException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
 
         } finally {
             final long time = System.currentTimeMillis() - start;
