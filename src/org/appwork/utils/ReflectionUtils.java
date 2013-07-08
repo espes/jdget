a4781b679e7faf518cdb0cf5dc3dca6628c5420c
Thomas <Thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-07-08 09:41:00 +0000

    fixes #8898
    git-svn-id: svn://svn.appwork.org/utils@1743 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/ReflectionUtils.java
+++ src/org/appwork/utils/ReflectionUtils.java
@@ -3,14 +3,22 @@ package org.appwork.utils;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.appwork.utils.reflection.Clazz;
 
 public class ReflectionUtils {
 
@@ -21,46 +29,54 @@ public class ReflectionUtils {
      * @param class1
      * @return
      */
-    public static <T> List<Class<? extends T>> getClassesInPackage(ClassLoader cl, String name, final Pattern pattern, Class<T> class1) {
+    public static <T> List<Class<? extends T>> getClassesInPackage(final ClassLoader cl, final String name, final Pattern pattern, final Class<T> class1) {
         Enumeration<URL> found;
-        List<T> ret = new ArrayList<T>();
+        final List<T> ret = new ArrayList<T>();
         try {
             final String finalName = name.replace(".", "/");
             found = cl.getResources(finalName);
 
             while (found.hasMoreElements()) {
 
-                URL url = found.nextElement();
-           
+                final URL url = found.nextElement();
+
                 if (url.getProtocol().equalsIgnoreCase("jar")) {
-                    String path = url.getPath();
-                    File jarFile = new File(new URL(path.substring(0, path.lastIndexOf('!'))).toURI());
+                    final String path = url.getPath();
+                    final File jarFile = new File(new URL(path.substring(0, path.lastIndexOf('!'))).toURI());
                     JarInputStream jis = null;
                     try {
                         jis = new JarInputStream(new FileInputStream(jarFile));
                         JarEntry e;
 
                         while ((e = jis.getNextJarEntry()) != null) {
-                           
-                            if (!e.getName().endsWith(".class")) continue;
-                            if (!e.getName().startsWith(finalName)) continue;
+
+                            if (!e.getName().endsWith(".class")) {
+                                continue;
+                            }
+                            if (!e.getName().startsWith(finalName)) {
+                                continue;
+                            }
                             // try {
                             if (pattern != null) {
 
-                                Matcher matcher = pattern.matcher(e.getName());
-                                if (!matcher.matches()) continue;
+                                final Matcher matcher = pattern.matcher(e.getName());
+                                if (!matcher.matches()) {
+                                    continue;
+                                }
 
                             }
 
                             String classPath = e.getName().replace("/", ".");
                             classPath = classPath.substring(0, classPath.length() - 6);
                             try {
-                                Class<?> clazz = cl.loadClass(classPath);
-                         if(class1==clazz)continue;
+                                final Class<?> clazz = cl.loadClass(classPath);
+                                if (class1 == clazz) {
+                                    continue;
+                                }
                                 if (class1 == null || class1.isAssignableFrom(clazz)) {
                                     ret.add((T) clazz);
                                 }
-                            } catch (Throwable ee) {
+                            } catch (final Throwable ee) {
 
                             }
 
@@ -72,46 +88,125 @@ public class ReflectionUtils {
                         }
                     }
                 } else {
-                    File path = new File(url.toURI());
-                    int i = path.getAbsolutePath().replace("\\", "/").indexOf(finalName);
+                    final File path = new File(url.toURI());
+                    final int i = path.getAbsolutePath().replace("\\", "/").indexOf(finalName);
                     final File root = new File(path.getAbsolutePath().substring(0, i));
-                    List<File> files = Files.getFiles(new FileFilter() {
+                    final List<File> files = Files.getFiles(new FileFilter() {
 
                         @Override
-                        public boolean accept(File pathname) {
-                            if (!pathname.getName().endsWith(".class")) return false;
-                            String rel = Files.getRelativePath(root, pathname);
+                        public boolean accept(final File pathname) {
+                            if (!pathname.getName().endsWith(".class")) { return false; }
+                            final String rel = Files.getRelativePath(root, pathname);
                             if (pattern != null) {
-                                Matcher matcher = pattern.matcher(rel);
-                                if (!matcher.matches()) return false;
+                                final Matcher matcher = pattern.matcher(rel);
+                                if (!matcher.matches()) { return false; }
                             }
 
                             return true;
                         }
                     }, new File(url.toURI()));
-                    
-                    for(File classFile:files){
+
+                    for (final File classFile : files) {
                         String classPath = Files.getRelativePath(root, classFile).replace("/", ".").replace("\\", ".");
                         classPath = classPath.substring(0, classPath.length() - 6);
                         try {
-                            Class<?> clazz = cl.loadClass(classPath);
-                            if(class1==clazz)continue;
+                            final Class<?> clazz = cl.loadClass(classPath);
+                            if (class1 == clazz) {
+                                continue;
+                            }
                             if (class1 == null || class1.isAssignableFrom(clazz)) {
                                 ret.add((T) clazz);
                             }
-                        } catch (Throwable ee) {
+                        } catch (final Throwable ee) {
 
                         }
-                        
+
                     }
                     //
                 }
             }
-        } catch (Exception e2) {
+        } catch (final Exception e2) {
             // TODO Auto-generated catch block
             e2.printStackTrace();
         }
         return (List<Class<? extends T>>) ret;
     }
 
+    /**
+     * @return
+     */
+    public static Collection<GetterSetter> getGettersSetteres(final Class<?> clazz) {
+        final HashMap<String, GetterSetter> map = new HashMap<String, GetterSetter>();
+
+        for (final Method m : clazz.getDeclaredMethods()) {
+            String key = null;
+            boolean getter = false;
+            if (m.getName().startsWith("is") && Clazz.isBoolean(m.getReturnType()) && m.getParameterTypes().length == 0) {
+                key = m.getName().substring(2);
+                getter = true;
+
+            } else if (m.getName().startsWith("get") && m.getParameterTypes().length == 0) {
+                key = m.getName().substring(3);
+                getter = true;
+
+            } else if (m.getName().startsWith("set") && m.getParameterTypes().length == 1) {
+                key = m.getName().substring(3);
+                getter = false;
+
+            }
+
+            if (StringUtils.isNotEmpty(key)) {
+                GetterSetter v = map.get(key);
+                if (v == null) {
+                    v = new GetterSetter(key);
+                    map.put(key, v);
+                }
+                if (getter) {
+                    v.setGetter(m);
+                } else {
+                    v.setSetter(m);
+                }
+                Field field;
+                try {
+                    field = clazz.getField(key.substring(0, 1).toLowerCase(Locale.ENGLISH) + key.substring(1));
+                    v.setField(field);
+                } catch (final NoSuchFieldException e) {
+                }
+
+            }
+        }
+        return map.values();
+    }
+
+    /**
+     * @param type
+     * @return
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     */
+    public static Object[] getEnumValues(final Class<? extends Enum> type) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+   
+            return (Object[]) type.getMethod("values", new Class[] {}).invoke(null, new Object[] {});
+    
+
+    }
+
+    /**
+     * @param type
+     * @param value
+     * @return
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     */
+    public static Object getEnumValueOf(final Class<? extends Enum> type, final String value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        return type.getMethod("valueOf", new Class[] { String.class }).invoke(null, new Object[] { value });
+        
+       
+    }
 }
