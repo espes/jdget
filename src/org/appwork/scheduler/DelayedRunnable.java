ef2956006cff3ca4fc9ea20bf1dc5a2b4c5c459f
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-07-23 10:52:17 +0000

    Rewrote DelayedRunnable: -use CAS synchs instead of bigger synchronized -fixed delayAgain calculation -avoid if/else branches where possible (maybe helps against guarantee(this->is8bit(imm8)) failed: Short forward jump exceeds) -added getID (for debugging)
    git-svn-id: svn://svn.appwork.org/utils@1792 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/scheduler/DelayedRunnable.java
+++ src/org/appwork/scheduler/DelayedRunnable.java
@@ -10,8 +10,9 @@
 package org.appwork.scheduler;
 
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author daniel
@@ -21,12 +22,12 @@ public abstract class DelayedRunnable implements Runnable {
 
     private final ScheduledExecutorService service;
     private final long                     delayInMS;
-    private long                           nextDelay;
-    private volatile long                  lastRunRequest  = 0;
-    private volatile long                  firstRunRequest = 0;
-    private ScheduledFuture<?>             delayer;
+    private final AtomicLong               nextDelay       = new AtomicLong(0);
+    private final AtomicLong               lastRunRequest  = new AtomicLong(0);
+    private final AtomicLong               firstRunRequest = new AtomicLong(0);
+    private final AtomicBoolean            delayerSet      = new AtomicBoolean(false);
     private final long                     maxInMS;
-    private boolean                        delayerEnabled  = true;
+    private final AtomicBoolean            delayerEnabled  = new AtomicBoolean(true);
 
     public DelayedRunnable(final ScheduledExecutorService service, final long delayInMS) {
         this(service, delayInMS, -1);
@@ -34,7 +35,8 @@ public abstract class DelayedRunnable implements Runnable {
 
     public DelayedRunnable(final ScheduledExecutorService service, final long minDelayInMS, final long maxDelayInMS) {
         this.service = service;
-        this.nextDelay = this.delayInMS = minDelayInMS;
+        this.delayInMS = minDelayInMS;
+        this.nextDelay.set(this.delayInMS);
         this.maxInMS = maxDelayInMS;
         if (this.delayInMS <= 0) { throw new IllegalArgumentException("minDelay must be >0"); }
         if (this.maxInMS == 0) { throw new IllegalArgumentException("maxDelay must be !=0"); }
@@ -42,8 +44,15 @@ public abstract class DelayedRunnable implements Runnable {
 
     abstract public void delayedrun();
 
+    /**
+     * @return the id
+     */
+    public String getID() {
+        return null;
+    }
+
     public boolean isDelayerEnabled() {
-        return this.delayerEnabled;
+        return this.delayerEnabled.get();
     }
 
     public void resetAndStart() {
@@ -52,86 +61,80 @@ public abstract class DelayedRunnable implements Runnable {
 
     @Override
     public void run() {
-        if (this.delayerEnabled == false) {
+        if (this.isDelayerEnabled() == false) {
             DelayedRunnable.this.delayedrun();
             return;
         }
-        synchronized (this) {
-            /* lastRunRequest is updated every time */
-            this.lastRunRequest = System.currentTimeMillis();
-            if (this.delayer == null) {
-                if (this.firstRunRequest == 0) {
-                    /* firstRunRequest is updated only once */
-                    this.firstRunRequest = System.currentTimeMillis();
+        this.lastRunRequest.set(System.currentTimeMillis());
+        if (this.delayerSet.getAndSet(true) == true) { return; }
+        this.firstRunRequest.compareAndSet(0, System.currentTimeMillis());
+        this.service.schedule(new Runnable() {
+
+            private void delayAgain(final long currentTime, Long nextDelay, final long minDif, final long thisRequestRun) {
+                if (DelayedRunnable.this.delayerSet.get() == false) { return; }
+                DelayedRunnable.this.firstRunRequest.set(currentTime);
+                if (nextDelay == null) {
+                    nextDelay = Math.max(0, DelayedRunnable.this.delayInMS - minDif);
                 }
-                this.delayer = this.service.schedule(new Runnable() {
-                    public void run() {
-                        boolean doWhat = false;
-                        synchronized (DelayedRunnable.this) {
-                            /* do we have to run now? */
-                            final long dif = System.currentTimeMillis() - DelayedRunnable.this.lastRunRequest;
-                            boolean runNow = dif >= DelayedRunnable.this.nextDelay;
-                            if (DelayedRunnable.this.maxInMS > 0) {
-                                /* is a maxDelay set? */
-                                if (System.currentTimeMillis() - DelayedRunnable.this.firstRunRequest > DelayedRunnable.this.maxInMS) {
-                                    /* we have to run now! */
-                                    runNow = true;
-                                } else {
-                                    /*
-                                     * calc new nextDelay so we can reach
-                                     * maxDelay better
-                                     */
-                                    DelayedRunnable.this.nextDelay = Math.max(DelayedRunnable.this.maxInMS - (System.currentTimeMillis() - DelayedRunnable.this.firstRunRequest), 10);
-                                }
-                            }
-                            DelayedRunnable.this.delayer = null;
-                            if (runNow) {
-                                /* we no longer delay the run */
-                                doWhat = true;
-                                /* reset nextDelay and firstRunRequest */
-                                DelayedRunnable.this.firstRunRequest = System.currentTimeMillis();
-                                DelayedRunnable.this.nextDelay = DelayedRunnable.this.delayInMS;
-                            } else {
-                                /* lets delay it again */
-                                DelayedRunnable.this.nextDelay = DelayedRunnable.this.delayInMS - dif;
-                                doWhat = false;
-                                DelayedRunnable.this.run();
-                            }
-                        }
-                        if (doWhat) {
-                            /* we no longer delay the run */
-                            DelayedRunnable.this.delayedrun();
-                        } else {
-                            /* lets delay it again */
-                            DelayedRunnable.this.run();
-                        }
-                    }
-
-                }, DelayedRunnable.this.nextDelay, TimeUnit.MILLISECONDS);
-
+                if (nextDelay < 10) {
+                    this.runNow(currentTime, thisRequestRun, minDif);
+                    return;
+                }
+                DelayedRunnable.this.service.schedule(this, nextDelay, TimeUnit.MILLISECONDS);
             }
-        }
+
+            public void run() {
+                if (DelayedRunnable.this.delayerSet.get() == false) { return; }
+                final long thisRunRequest = DelayedRunnable.this.lastRunRequest.get();
+                final long currentTime = System.currentTimeMillis();
+                final long minDif = currentTime - thisRunRequest;
+                if (minDif >= DelayedRunnable.this.delayInMS) {
+                    /* minDelay reached, run now */
+                    this.runNow(currentTime, thisRunRequest, minDif);
+                    return;
+                }
+                final long firstRunRequest = DelayedRunnable.this.firstRunRequest.get();
+                Long nextDelay = null;
+                if (DelayedRunnable.this.maxInMS > 0) {
+                    final long maxDif = currentTime - firstRunRequest;
+                    if (maxDif >= DelayedRunnable.this.maxInMS) {
+                        /* maxDelay reached, run now */
+                        this.runNow(currentTime, thisRunRequest, minDif);
+                        return;
+                    }
+                    final long delay = DelayedRunnable.this.maxInMS - maxDif;
+                    nextDelay = Math.min(delay, DelayedRunnable.this.delayInMS);
+                }
+                this.delayAgain(currentTime, nextDelay, minDif, thisRunRequest);
+            }
+
+            private void runNow(final long currentTime, final long thisRunRequest, final long minDif) {
+                DelayedRunnable.this.delayedrun();
+                if (thisRunRequest != DelayedRunnable.this.lastRunRequest.get()) {
+                    this.delayAgain(currentTime, DelayedRunnable.this.delayInMS, minDif, thisRunRequest);
+                } else {
+                    this.stop();
+                }
+            }
+
+            private void stop() {
+                DelayedRunnable.this.nextDelay.set(DelayedRunnable.this.delayInMS);
+                DelayedRunnable.this.firstRunRequest.set(0);
+                DelayedRunnable.this.delayerSet.set(false);
+            }
+
+        }, DelayedRunnable.this.delayInMS, TimeUnit.MILLISECONDS);
     }
 
     public void setDelayerEnabled(final boolean b) {
-        if (this.delayerEnabled == b) { return; }
-        synchronized (this) {
-            if (b == false) {
-                this.stop();
-            }
-            this.delayerEnabled = b;
+        if (this.delayerEnabled.getAndSet(b) == b) { return; }
+        if (!b) {
+            this.stop();
         }
     }
 
-    public boolean stop() {
-        synchronized (this) {
-            if (this.delayer != null) {
-                this.delayer.cancel(false);
-                this.delayer = null;
-                return true;
-            }
-        }
-        return false;
+    public void stop() {
+        this.delayerSet.set(false);
     }
 
 }
