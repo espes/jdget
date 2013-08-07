642bb67cb4a5acc39ec16d27da1009a7b3c9eaea
Thomas <Thomas@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-08-07 09:24:55 +0000

    git-svn-id: svn://svn.appwork.org/utils@1825 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/utils/swing/dialog/Dialog.java
+++ src/org/appwork/utils/swing/dialog/Dialog.java
@@ -244,7 +244,7 @@ public class Dialog {
 
     private Dialog() {
 
-        this.defaultHandler = new DialogHandler() {
+        defaultHandler = new DialogHandler() {
 
             @Override
             public <T> T showDialog(final AbstractDialog<T> dialog) throws DialogClosedException, DialogCanceledException {
@@ -254,7 +254,7 @@ public class Dialog {
     }
 
     public DialogHandler getDefaultHandler() {
-        return this.defaultHandler;
+        return defaultHandler;
     }
 
     /**
@@ -262,23 +262,23 @@ public class Dialog {
      * @see Dialog#defaultTimeout
      */
     protected int getDefaultTimeout() {
-        return this.defaultTimeout;
+        return defaultTimeout;
     }
 
     public DialogHandler getHandler() {
-        return this.handler;
+        return handler;
     }
 
     /**
      * @return
      */
     public List<? extends Image> getIconList() {
-        return this.iconList;
+        return iconList;
     }
 
     public LAFManagerInterface getLafManager() {
         synchronized (this) {
-            return this.lafManager;
+            return lafManager;
         }
     }
 
@@ -287,9 +287,9 @@ public class Dialog {
      */
     public void initLaf() {
         synchronized (this) {
-            if (this.lafManager != null) {
-                this.lafManager.init();
-                this.setLafManager(null);
+            if (lafManager != null) {
+                lafManager.init();
+                setLafManager(null);
             }
         }
     }
@@ -300,12 +300,12 @@ public class Dialog {
      * @see Dialog#defaultTimeout
      */
     public void setDefaultTimeout(final int countdownTime) {
-        this.defaultTimeout = countdownTime;
+        defaultTimeout = countdownTime;
     }
 
     public void setHandler(DialogHandler handler) {
         if (handler == null) {
-            handler = this.defaultHandler;
+            handler = defaultHandler;
         }
         this.handler = handler;
     }
@@ -472,7 +472,7 @@ public class Dialog {
      * @throws DialogCanceledException
      */
     public <T> T showDialog(final AbstractDialog<T> dialog) throws DialogClosedException, DialogCanceledException {
-        final DialogHandler lhandler = this.handler;
+        final DialogHandler lhandler = handler;
         if (lhandler != null) { return lhandler.showDialog(dialog); }
         return this.showDialogRaw(dialog);
     }
@@ -526,31 +526,15 @@ public class Dialog {
             }
 
         };
-        boolean interrupted = false;
+     
         try {
-            if (Thread.interrupted()) {
-                new EDTRunner() {
-
-                    @Override
-                    protected void runInEDT() {
-                        try {
-                            // close dialog if open
-                            dialog.interrupt();
-                        } catch (final Exception e) {
-                        }
-                    }
-                };
-                throw new DialogClosedException(Dialog.RETURN_INTERRUPT, new InterruptedException());
-            }
+            if (Thread.interrupted()) { throw new InterruptedException(); }
             synchronized (waitingLock) {
                 if (waitingLock.get() == false) {
                     waitingLock.wait();
                 }
             }
         } catch (final InterruptedException e) {
-            interrupted = true;
-        }
-        if (edth.isInterrupted() || interrupted) {
             // Use a edtrunner here. AbstractCaptcha.dispose is edt save...
             // however there may be several CaptchaDialog classes with
             // overriddden unsave dispose methods...
@@ -566,12 +550,13 @@ public class Dialog {
                 }
             };
             try {
-                throw new DialogClosedException(dialog.getReturnmask() | Dialog.RETURN_INTERRUPT, edth.getInterruptException());
-            } catch (final IllegalStateException e) {
+                throw new DialogClosedException(dialog.getReturnmask() | Dialog.RETURN_INTERRUPT, e);
+            } catch (final IllegalStateException e1) {
                 // if we cannot get the returnmask from the dialog
-                throw new DialogClosedException(Dialog.RETURN_INTERRUPT, new InterruptedException());
+                throw new DialogClosedException(Dialog.RETURN_INTERRUPT, e);
             }
         }
+
         final T ret = dialog.getReturnValue();
         final int mask = dialog.getReturnmask();
         if (BinaryLogic.containsSome(mask, Dialog.RETURN_CLOSED)) { throw new DialogClosedException(mask); }
