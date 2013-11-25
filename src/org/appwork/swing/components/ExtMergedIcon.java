03305b4316ff62fbcdd01aa2393c79c40b13ed4b
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2013-11-25 11:48:03 +0000

    git-svn-id: svn://svn.appwork.org/utils@1910 21714237-3853-44ef-a1f0-ef8f03a7d1fe

--- src/org/appwork/swing/components/ExtMergedIcon.java
+++ src/org/appwork/swing/components/ExtMergedIcon.java
@@ -16,42 +16,41 @@ import org.appwork.utils.ImageProvider.ImageProvider;
 public class ExtMergedIcon implements Icon {
     private class Entry {
 
-        private Icon      icon;
-        private int       x;
-        private int       y;
-        private Composite composite;
-        private int       z;
+        private final Icon      icon;
+        private final int       x;
+        private final int       y;
+        private final Composite composite;
+        private final int       z;
 
         public Entry(final Icon icon, final int x, final int y, final int z, final Composite c) {
             this.icon = icon;
             this.x = x;
             this.y = y;
-            composite = c;
+            this.composite = c;
             this.z = z;
         }
 
     }
-    private int cropedWidth=-1;
-    private int cropedHeight=-1;
+
+    private int                  cropedWidth  = -1;
+    private int                  cropedHeight = -1;
+
+    private final TreeSet<Entry> entries      = new TreeSet<Entry>(new Comparator<Entry>() {
+
+                                                  @Override
+                                                  public int compare(final Entry o1, final Entry o2) {
+                                                      return new Integer(o1.z).compareTo(new Integer(o2.z));
+                                                  }
+                                              });
+
+    private int                  width        = 0;
+    private int                  height       = 0;
+    private ImageIcon            internalIcon;
+
+    private boolean              caching;
 
     public ExtMergedIcon() {
 
-    }
-
-    public ExtMergedIcon add(final Icon icon, final int x, final int y, final int z, final Composite c) {
-        addEntry(new Entry(icon, x, y, z, c));
-        return this;
-    }
-    /**
-     * @param abstractIcon
-     * @param i
-     * @param j
-     */
-    public ExtMergedIcon(final Icon icon, final int x, final int y) {
-        addEntry(new Entry(icon, x, y, 0 , null));
-    }
-    public ExtMergedIcon(final Icon icon, final int x, final int y, final int z, final Composite c) {
-        addEntry(new Entry(icon, x, y, z, c));
     }
 
     /**
@@ -61,61 +60,106 @@ public class ExtMergedIcon implements Icon {
         this(icon, 0, 0, 0, null);
     }
 
+    /**
+     * @param abstractIcon
+     * @param i
+     * @param j
+     */
+    public ExtMergedIcon(final Icon icon, final int x, final int y) {
+        this.addEntry(new Entry(icon, x, y, 0, null));
+    }
 
-    public ExtMergedIcon crop(final int width, final int height) {
-        cropedWidth=width;
-        cropedHeight=height;
-        
+    public ExtMergedIcon(final Icon icon, final int x, final int y, final int z, final Composite c) {
+        this.addEntry(new Entry(icon, x, y, z, c));
+    }
+
+    public ExtMergedIcon add(final Icon icon) {
+        synchronized (this.entries) {
+            return this.add(icon, 0, 0, this.entries.size(), null);
+        }
+    }
+
+    /**
+     * @param abstractIcon
+     * @param i
+     * @param j
+     * @return
+     */
+    public ExtMergedIcon add(final Icon icon, final int x, final int y) {
+        synchronized (this.entries) {
+            return this.add(icon, x, y, this.entries.size(), null);
+        }
+
+    }
+
+    public ExtMergedIcon add(final Icon icon, final int x, final int y, final int z, final Composite c) {
+        this.addEntry(new Entry(icon, x, y, z, c));
         return this;
     }
-    private TreeSet<Entry> entries = new TreeSet<Entry>(new Comparator<Entry>() {
-
-                                       @Override
-                                       public int compare(final Entry o1, final Entry o2) {
-                                           return new Integer(o1.z).compareTo(new Integer(o2.z));
-                                       }
-                                   });
-    private int            width   = 0;
-    private int            height  = 0;
-    private ImageIcon      internalIcon;
-    private boolean        caching;
 
     private void addEntry(final Entry entry) {
-        if (internalIcon != null) { throw new IllegalStateException("internalIcon is set"); }
-        width = Math.max(width, entry.x + entry.icon.getIconWidth());
-        height = Math.max(height, entry.y + entry.icon.getIconHeight());
-        entries.add(entry);
+        if (this.internalIcon != null) { throw new IllegalStateException("internalIcon is set"); }
+        this.width = Math.max(this.width, entry.x + entry.icon.getIconWidth());
+        this.height = Math.max(this.height, entry.y + entry.icon.getIconHeight());
+        this.entries.add(entry);
     }
 
     public void cache() {
-        caching = true;
+        this.caching = true;
         try {
-            internalIcon = ImageProvider.toImageIcon(this);
-            entries.clear();
+            this.internalIcon = ImageProvider.toImageIcon(this);
+            this.entries.clear();
         } finally {
-            caching = false;
+            this.caching = false;
         }
+    }
+
+    /**
+     * @return
+     */
+    public Icon crop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ExtMergedIcon crop(final int width, final int height) {
+        this.cropedWidth = width;
+        this.cropedHeight = height;
+
+        return this;
+    }
+
+    @Override
+    public int getIconHeight() {
+        if (this.cropedHeight > 0) { return this.cropedHeight; }
+        return this.height;
+    }
+
+    @Override
+    public int getIconWidth() {
+        if (this.cropedWidth > 0) { return this.cropedWidth; }
+        return this.width;
     }
 
     @Override
     public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
 
         final Graphics2D g2 = (Graphics2D) g;
- 
-        if (internalIcon == null && !caching) {
-            cache();
+
+        if (this.internalIcon == null && !this.caching) {
+            this.cache();
 
         }
 
-        if (internalIcon != null) {
-            g2.drawImage(internalIcon.getImage(), x, y, null);
+        if (this.internalIcon != null) {
+            g2.drawImage(this.internalIcon.getImage(), x, y, null);
             // internalIcon.paintIcon(c, g2, x, y);
             return;
         }
         final Shape oldClip = g2.getClip();
-//        Rectangle rec = new Rectangle( );
-        g2.setClip(x, y, getIconWidth(),getIconHeight());
-        for (final Entry e : entries) {
+        // Rectangle rec = new Rectangle( );
+        g2.setClip(x, y, this.getIconWidth(), this.getIconHeight());
+        for (final Entry e : this.entries) {
             final Composite com = g2.getComposite();
             try {
                 if (e.composite != null) {
@@ -130,49 +174,6 @@ public class ExtMergedIcon implements Icon {
 
         }
         g2.setClip(oldClip);
-    }
-
-    @Override
-    public int getIconWidth() {
-        if(cropedWidth>0) {
-            return cropedWidth;
-        }
-        return width;
-    }
-
-    @Override
-    public int getIconHeight() {
-        if(cropedHeight>0) {
-            return cropedHeight;
-        }
-        return height;
-    }
-    public ExtMergedIcon add(final Icon icon) {
-        synchronized (entries) {
-
-            return this.add(icon,0, 0, entries.size(), null);
-        }
-    }
-    /**
-     * @param abstractIcon
-     * @param i
-     * @param j
-     * @return
-     */
-    public ExtMergedIcon add(final Icon icon, final int x, final int y) {
-        synchronized (entries) {
-
-            return this.add(icon, x, y, entries.size(), null);
-        }
-
-    }
-
-    /**
-     * @return
-     */
-    public Icon crop() {
-        // TODO Auto-generated method stub
-        return null;
     }
 
 }
