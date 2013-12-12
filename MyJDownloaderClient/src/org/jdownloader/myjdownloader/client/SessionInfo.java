c9dea61fe34f52c4569dcc5ea94f2cb81bb01a4a
kotz <kotz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-12-12 09:37:54 +0000

    SessionInfo: equals + hashCode overriden
    git-svn-id: svn://svn.jdownloader.org/jdownloader@23547 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/SessionInfo.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/SessionInfo.java
@@ -4,11 +4,8 @@ public class SessionInfo {
 
     private byte[] deviceSecret;
     private byte[] deviceEncryptionToken;
-
     private byte[] serverEncryptionToken;
-
     private String sessionToken;
-
     private String regainToken;
 
     public SessionInfo(/* STorable */) {
@@ -23,40 +20,55 @@ public class SessionInfo {
         this.regainToken = regainToken;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o == null) {
+            return false;
+        } else if (this.getClass() != o.getClass()) { return false; }
+        return ((SessionInfo) o).getSessionToken().equals(this.getSessionToken());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.sessionToken.hashCode();
+    }
+
     public byte[] getDeviceEncryptionToken() {
         return this.deviceEncryptionToken;
-    }
-
-    public byte[] getDeviceSecret() {
-        return this.deviceSecret;
-    }
-
-    public String getRegainToken() {
-        return this.regainToken;
-    }
-
-    public byte[] getServerEncryptionToken() {
-        return this.serverEncryptionToken;
-    }
-
-    public String getSessionToken() {
-        return this.sessionToken;
     }
 
     public void setDeviceEncryptionToken(final byte[] deviceEncryptionToken) {
         this.deviceEncryptionToken = deviceEncryptionToken;
     }
 
+    public byte[] getDeviceSecret() {
+        return this.deviceSecret;
+    }
+
     public void setDeviceSecret(final byte[] deviceSecret) {
         this.deviceSecret = deviceSecret;
+    }
+
+    public String getRegainToken() {
+        return this.regainToken;
     }
 
     public void setRegainToken(final String regainToken) {
         this.regainToken = regainToken;
     }
 
+    public byte[] getServerEncryptionToken() {
+        return this.serverEncryptionToken;
+    }
+
     public void setServerEncryptionToken(final byte[] serverEncryptionToken) {
         this.serverEncryptionToken = serverEncryptionToken;
+    }
+
+    public String getSessionToken() {
+        return this.sessionToken;
     }
 
     public void setSessionToken(final String sessionToken) {
