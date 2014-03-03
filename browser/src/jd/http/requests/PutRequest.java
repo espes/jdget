e05439f5d433050ed526a82ee4d94d1f8deaf2b6
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-03 13:09:06 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@24514 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ browser/src/jd/http/requests/PutRequest.java
@@ -0,0 +1,30 @@
+package jd.http.requests;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import jd.http.Request;
+import jd.parser.html.Form;
+
+import org.appwork.utils.net.httpconnection.HTTPConnection.RequestMethod;
+
+public class PutRequest extends PostRequest {
+    
+    public PutRequest(final Form form) throws MalformedURLException {
+        super(form);
+    }
+    
+    public PutRequest(final Request cloneRequest) {
+        super(cloneRequest);
+    }
+    
+    public PutRequest(final String url) throws MalformedURLException {
+        super(url);
+    }
+    
+    @Override
+    public void preRequest() throws IOException {
+        super.preRequest();
+        this.httpConnection.setRequestMethod(RequestMethod.PUT);
+    }
+}
