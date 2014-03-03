52ad8b74b657cfb197dbb0ec01738b120cf36b1e
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-03 16:16:11 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@24518 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/StorageMyJDClient.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/StorageMyJDClient.java
@@ -19,13 +19,13 @@ import org.jdownloader.myjdownloader.client.json.storage.StorageGetValueResponse
 import org.jdownloader.myjdownloader.client.json.storage.StorageListResponse;
 
 public class StorageMyJDClient<GenericType> {
-
+    
     private final AbstractMyJDClient<GenericType> api;
-
+    
     public StorageMyJDClient(final AbstractMyJDClient<GenericType> abstractMyJDClient) {
         this.api = abstractMyJDClient;
     }
-
+    
     protected <T> T callServer(final String query, final JSonRequest jsonRequest, final SessionInfo session, final Class<T> class1) throws MyJDownloaderException {
         try {
             return this.api.callServer(query, jsonRequest, session, class1);
@@ -39,29 +39,29 @@ public class StorageMyJDClient<GenericType> {
                 }
                 if (error != null) {
                     switch (error.getSrc()) {
-                    case MYJD:
-                        final ServerErrorType type = ServerErrorType.valueOf(error.getType());
-                        switch (type) {
-                        case STORAGE_ALREADY_EXISTS:
-                            throw new StorageAlreadyExistsException();
-                        case STORAGE_INVALID_KEY:
-                            throw new StorageInvalidKeyException();
-                        case STORAGE_INVALID_STORAGEID:
-                            throw new StorageInvalidIDException();
-                        case STORAGE_NOT_FOUND:
-                            throw new StorageNotFoundException();
-                        case STORAGE_KEY_NOT_FOUND:
-                            throw new StorageKeyNotFoundException();
-                        case STORAGE_LIMIT_REACHED:
-                            throw new StorageLimitReachedException();
-                        }
+                        case MYJD:
+                            final ServerErrorType type = ServerErrorType.valueOf(error.getType());
+                            switch (type) {
+                                case STORAGE_ALREADY_EXISTS:
+                                    throw new StorageAlreadyExistsException();
+                                case STORAGE_INVALID_KEY:
+                                    throw new StorageInvalidKeyException();
+                                case STORAGE_INVALID_STORAGEID:
+                                    throw new StorageInvalidIDException();
+                                case STORAGE_NOT_FOUND:
+                                    throw new StorageNotFoundException();
+                                case STORAGE_KEY_NOT_FOUND:
+                                    throw new StorageKeyNotFoundException();
+                                case STORAGE_LIMIT_REACHED:
+                                    throw new StorageLimitReachedException();
+                            }
                     }
                 }
             }
             throw e;
         }
     }
-
+    
     public void create(final String storageID) throws MyJDownloaderException, StorageAlreadyExistsException, StorageInvalidIDException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/createstorage?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
@@ -70,7 +70,7 @@ public class StorageMyJDClient<GenericType> {
         re.setUrl(url);
         this.callServer(url, re, sessionInfo, RequestIDOnly.class);
     }
-
+    
     public void drop(final String storageID) throws MyJDownloaderException, StorageNotFoundException, StorageInvalidIDException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/dropstorage?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
@@ -79,7 +79,7 @@ public class StorageMyJDClient<GenericType> {
         request.setUrl(url);
         this.callServer(url, request, sessionInfo, RequestIDOnly.class);
     }
-
+    
     public String getValue(final String storageID, final String key) throws MyJDownloaderException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/getvalue?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
@@ -88,13 +88,13 @@ public class StorageMyJDClient<GenericType> {
         request.setUrl(url);
         return this.callServer(url, request, sessionInfo, StorageGetValueResponse.class).getValue();
     }
-
+    
     public Map<String, Long> list() throws MyJDownloaderException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/liststorages?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
         return this.callServer(url, null, sessionInfo, StorageListResponse.class).getList();
     }
-
+    
     public Map<String, Long> listKeys(final String storageID) throws MyJDownloaderException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/listkeys?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
@@ -103,7 +103,7 @@ public class StorageMyJDClient<GenericType> {
         request.setUrl(url);
         return this.callServer(url, request, sessionInfo, StorageListResponse.class).getList();
     }
-
+    
     public void putValue(final String storageID, final String key, final String value) throws MyJDownloaderException {
         final SessionInfo sessionInfo = this.api.getSessionInfo();
         final String url = "/storage/putvalue?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
@@ -111,7 +111,15 @@ public class StorageMyJDClient<GenericType> {
         request.setParams(new Object[] { storageID, key, value });
         request.setUrl(url);
         this.callServer(url, request, sessionInfo, RequestIDOnly.class);
-
     }
-
+    
+    public void removeKey(final String storageID, final String key) throws MyJDownloaderException {
+        final SessionInfo sessionInfo = this.api.getSessionInfo();
+        final String url = "/storage/removekey?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
+        final JSonRequest request = new JSonRequest();
+        request.setParams(new Object[] { storageID, key });
+        request.setUrl(url);
+        this.callServer(url, request, sessionInfo, RequestIDOnly.class);
+    }
+    
 }
