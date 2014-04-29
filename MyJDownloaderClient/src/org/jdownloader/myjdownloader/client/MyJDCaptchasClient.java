ec3b21e5240e5424566821be2d827bad4215fa34
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-04-29 13:47:37 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@25200 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/MyJDCaptchasClient.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/MyJDCaptchasClient.java
@@ -7,6 +7,7 @@ import org.jdownloader.myjdownloader.client.json.JSonRequest;
 import org.jdownloader.myjdownloader.client.json.MyCaptchaChallenge;
 import org.jdownloader.myjdownloader.client.json.MyCaptchaSolution;
 import org.jdownloader.myjdownloader.client.json.MyCaptchaSolutionsListResponse;
+import org.jdownloader.myjdownloader.client.json.SuccessfulResponse;
 
 public class MyJDCaptchasClient<GenericType> {
     
@@ -16,14 +17,13 @@ public class MyJDCaptchasClient<GenericType> {
         this.api = abstractMyJDClient;
     }
     
+    public boolean abort(final String ID) throws MyJDownloaderException {
+        return this.remove(ID, MyCaptchaSolution.RESULT.ABORT);
+    }
+    
     public MyCaptchaSolution get(final String ID) throws MyJDownloaderException {
-        final SessionInfo sessionInfo = this.api.getSessionInfo();
-        final String url = "/my/captchas/get?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
-        final JSonRequest re = new JSonRequest();
-        re.setParams(new Object[] { new String[] { ID } });
-        re.setUrl(url);
-        final List<MyCaptchaSolution> list = this.api.callServer(url, re, sessionInfo, MyCaptchaSolutionsListResponse.class).getList();
-        if (list != null && list.size() == 1) { return list.get(0); }
+        final List<MyCaptchaSolution> ret = this.get(new String[] { ID });
+        if (ret != null && ret.size() == 1) { return ret.get(0); }
         return null;
     }
     
@@ -37,8 +37,21 @@ public class MyJDCaptchasClient<GenericType> {
         return list;
     }
     
+    public boolean invalidate(final String ID) throws MyJDownloaderException {
+        return this.remove(ID, MyCaptchaSolution.RESULT.WRONG);
+    }
+    
     public List<MyCaptchaSolution> list() throws MyJDownloaderException {
         return this.get((String[]) null);
+    }
+    
+    public boolean remove(final String ID, final MyCaptchaSolution.RESULT result) throws MyJDownloaderException {
+        final SessionInfo sessionInfo = this.api.getSessionInfo();
+        final String url = "/my/captchas/remove?sessiontoken=" + this.api.urlencode(sessionInfo.getSessionToken());
+        final JSonRequest re = new JSonRequest();
+        re.setParams(new Object[] { ID, result });
+        re.setUrl(url);
+        return this.api.callServer(url, re, sessionInfo, SuccessfulResponse.class).isSuccessful();
     }
     
     public MyCaptchaSolution solve(final MyCaptchaChallenge myCaptchaChallenge) throws MyJDownloaderException {
@@ -50,6 +63,14 @@ public class MyJDCaptchasClient<GenericType> {
         final List<MyCaptchaSolution> list = this.api.callServer(url, re, sessionInfo, MyCaptchaSolutionsListResponse.class).getList();
         if (list != null && list.size() == 1) { return list.get(0); }
         return null;
+    }
+    
+    public boolean timeout(final String ID) throws MyJDownloaderException {
+        return this.remove(ID, MyCaptchaSolution.RESULT.TIMEOUT);
+    }
+    
+    public boolean validate(final String ID) throws MyJDownloaderException {
+        return this.remove(ID, MyCaptchaSolution.RESULT.CORRECT);
     }
     
 }
